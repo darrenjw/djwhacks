20dd6841a61bf7e3fca02b8b9559f8505f2907a7
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2014-02-16 20:33:12 +0000

    messing with parallel monte carlo in scala

---
+++ scala/monte-carlo/monte-carlo.scala
@@ -0,0 +1,33 @@
+/*
+monte-carlo.scala
+
+scalac monte-carlo.scala
+time scala MonteCarlo
+
+*/
+
+import scala.util.Random.nextDouble
+import scala.math.exp
+import scala.annotation.tailrec
+
+object MonteCarlo {
+
+  @tailrec
+  def sum(its: Long,acc: Double): Double = {
+    if (its==0) 
+      (acc)
+    else {
+      val u=nextDouble
+      sum(its-1,acc+exp(-u*u))
+    }
+  }
+
+  def main(args: Array[String]) = {
+    println("Hello")
+    val iters=1000000000
+    val result=sum(iters,0.0)
+    println(result/iters)
+    println("Goodbye")
+  }
+
+}
---
+++ scala/monte-carlo/monte-carlo2.scala
@@ -0,0 +1,36 @@
+/*
+monte-carlo2.scala
+
+scalac monte-carlo2.scala
+time scala MonteCarlo
+
+*/
+
+import java.util.concurrent.ThreadLocalRandom
+import scala.math.exp
+import scala.annotation.tailrec
+
+object MonteCarlo {
+
+  @tailrec
+  def sum(its: Long,acc: Double): Double = {
+    if (its==0) 
+      (acc)
+    else {
+      val u=ThreadLocalRandom.current().nextDouble()
+      sum(its-1,acc+exp(-u*u))
+    }
+  }
+
+  def main(args: Array[String]) = {
+    println("Hello")
+    val N=4
+    val iters=1000000000
+    val its=iters/N
+    val sums=(1 to N).toList.par map {x => sum(its,0.0)}
+    val result=sums.reduce(_+_)
+    println(result/iters)
+    println("Goodbye")
+  }
+
+}

773d17e8d5a49f1f5e07d9e2aee446c55519b0c6
Darren Wilkinson <darren.wilkinson@ncl.ac.uk>
2014-02-16 21:52:41 +0000

    parametrised version

--- scala/monte-carlo/monte-carlo4.scala
+++ scala/monte-carlo/monte-carlo4.scala
@@ -1,7 +1,7 @@
 /*
-monte-carlo.scala
+monte-carlo4.scala
 
-scalac monte-carlo.scala
+scalac monte-carlo4.scala
 time scala MonteCarlo
 
 */
@@ -24,8 +24,11 @@ object MonteCarlo {
 
   def main(args: Array[String]) = {
     println("Hello")
+    val N=args(0).toInt
     val iters=100000000
-    val result=sum(iters,0.0)
+    val its=iters/N
+    val sums=(1 to N).toList.par map {x => sum(its,0.0)}
+    val result=sums.reduce(_+_)
     println(result/iters)
     println("Goodbye")
   }

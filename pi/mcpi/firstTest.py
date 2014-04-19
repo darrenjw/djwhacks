d5de5d73170b915de75ec63f1169c4a8b6b7ae8c
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2014-04-19 18:15:24 +0000

    added circles and cylinders

--- pi/mcpi/firstTest.py
+++ pi/mcpi/firstTest.py
@@ -2,6 +2,7 @@
 
 import minecraft
 import block
+import math
 
 mc=minecraft.Minecraft.create()
 mc.setBlock(20,20,20,block.COBBLESTONE)
@@ -26,6 +27,26 @@ def pyramid(sx,sy,sz,l,b):
     square(sx,sy,sz,l,b)
     pyramid(sx+1,sy+1,sz+1,l-2,b)
 
+def circle(cx,cy,cz,r,b):
+  x=r
+  y=0
+  while (x>=y):
+    mc.setBlock(cx+x,cy,cz+y,b)
+    mc.setBlock(cx+x,cy,cz-y,b)
+    mc.setBlock(cx-x,cy,cz+y,b)
+    mc.setBlock(cx-x,cy,cz-y,b)
+    mc.setBlock(cx+y,cy,cz+x,b)
+    mc.setBlock(cx+y,cy,cz-x,b)
+    mc.setBlock(cx-y,cy,cz+x,b)
+    mc.setBlock(cx-y,cy,cz-x,b)
+    y=y+1
+    x=int(round(math.sqrt(r*r-y*y)))
+
+def cylinder(cx,cy,cz,r,h,b):
+  if (h>0):
+    circle(cx,cy,cz,r,b)
+    cylinder(cx,cy+1,cz,r,h-1,b)
+
 line(20,20,20,1,0,0,30,block.COBBLESTONE)
 line(25,25,25,0,0,1,5,block.WOOL)
 
@@ -33,9 +54,11 @@ rect(35,35,35,10,5,block.DIRT)
 
 square(40,40,40,10,block.DIRT)
 
-pyramid(40,40,40,50,block.COBBLESTONE)
+pyramid(90,6,120,10,block.COBBLESTONE)
 
-
+circle(40,20,40,20,block.DIRT)
+circle(40,20,40,10,block.DIRT)
+cylinder(40,20,40,5,20,block.COBBLESTONE)
 
 
 # eof

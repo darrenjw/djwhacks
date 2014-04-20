0b26865012bd47c3f9718d1b28bc65840fbec712
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2014-04-20 19:14:28 +0000

    simple menger sponge renderer working

--- pi/mcpi/menger.py
+++ pi/mcpi/menger.py
@@ -4,7 +4,7 @@ from myMinecraft import *
 
 def menger(x,y,z,l):
   if (l==0):
-    setBlock(x,y,z,block.STONE)
+    mc.setBlock(x,y,z,block.STONE)
   else:
     s=3**(l-1)
     menger(x+0*s,y+0*s,z+0*s,l-1)
@@ -21,15 +21,15 @@ def menger(x,y,z,l):
     menger(x+1*s,y+0*s,z+2*s,l-1)
     #menger(x+1*s,y+1*s,z+0*s,l-1)
     #menger(x+1*s,y+1*s,z+1*s,l-1)
-    menger(x+1*s,y+1*s,z+2*s,l-1)
-    #menger(x+1*s,y+2*s,z+0*s,l-1)
-    menger(x+1*s,y+2*s,z+1*s,l-1)
+    #menger(x+1*s,y+1*s,z+2*s,l-1)
+    menger(x+1*s,y+2*s,z+0*s,l-1)
+    #menger(x+1*s,y+2*s,z+1*s,l-1)
     menger(x+1*s,y+2*s,z+2*s,l-1)
     menger(x+2*s,y+0*s,z+0*s,l-1)
-    #menger(x+2*s,y+0*s,z+1*s,l-1)
+    menger(x+2*s,y+0*s,z+1*s,l-1)
     menger(x+2*s,y+0*s,z+2*s,l-1)
-    #menger(x+2*s,y+1*s,z+0*s,l-1)
-    menger(x+2*s,y+1*s,z+1*s,l-1)
+    menger(x+2*s,y+1*s,z+0*s,l-1)
+    #menger(x+2*s,y+1*s,z+1*s,l-1)
     menger(x+2*s,y+1*s,z+2*s,l-1)
     menger(x+2*s,y+2*s,z+0*s,l-1)
     menger(x+2*s,y+2*s,z+1*s,l-1)

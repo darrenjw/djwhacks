7a638b46d384abb3c7196c865a2fa238f0d57e8f
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2014-04-20 15:08:19 +0100

    split out functions into a separate file

--- pi/mcpi/firstTest.py
+++ pi/mcpi/firstTest.py
@@ -1,51 +1,9 @@
 #!/usr/bin/env python
 
-import minecraft
-import block
-import math
+from myMinecraft import *
 
-mc=minecraft.Minecraft.create()
 mc.setBlock(20,20,20,block.COBBLESTONE)
 mc.setBlock(minecraft.Vec3(21,21,21),block.DIRT)
-
-def line(sx,sy,sz,ix,iy,iz,l,b):
-  if l>0:
-    mc.setBlock(sx,sy,sz,b)
-    line(sx+ix,sy+iy,sz+iz,ix,iy,iz,l-1,b)
-
-def rect(sx,sy,sz,lx,lz,b):
-  line(sx,sy,sz,1,0,0,lx,b)
-  line(sx,sy,sz,0,0,1,lz,b)
-  line(sx,sy,sz+lz-1,1,0,0,lx,b)
-  line(sx+lx-1,sy,sz,0,0,1,lz,b)
-
-def square(sx,sy,sz,l,b):
-  rect(sx,sy,sz,l,l,b)
-
-def pyramid(sx,sy,sz,l,b):
-  if (l>0):
-    square(sx,sy,sz,l,b)
-    pyramid(sx+1,sy+1,sz+1,l-2,b)
-
-def circle(cx,cy,cz,r,b):
-  x=r
-  y=0
-  while (x>=y):
-    mc.setBlock(cx+x,cy,cz+y,b)
-    mc.setBlock(cx+x,cy,cz-y,b)
-    mc.setBlock(cx-x,cy,cz+y,b)
-    mc.setBlock(cx-x,cy,cz-y,b)
-    mc.setBlock(cx+y,cy,cz+x,b)
-    mc.setBlock(cx+y,cy,cz-x,b)
-    mc.setBlock(cx-y,cy,cz+x,b)
-    mc.setBlock(cx-y,cy,cz-x,b)
-    y=y+1
-    x=int(round(math.sqrt(r*r-y*y)))
-
-def cylinder(cx,cy,cz,r,h,b):
-  if (h>0):
-    circle(cx,cy,cz,r,b)
-    cylinder(cx,cy+1,cz,r,h-1,b)
 
 line(20,20,20,1,0,0,30,block.COBBLESTONE)
 line(25,25,25,0,0,1,5,block.WOOL)
---
+++ pi/mcpi/myMinecraft.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import minecraft
+import block
+import math
+
+mc=minecraft.Minecraft.create()
+
+def line(sx,sy,sz,ix,iy,iz,l,b):
+  if l>0:
+    mc.setBlock(sx,sy,sz,b)
+    line(sx+ix,sy+iy,sz+iz,ix,iy,iz,l-1,b)
+
+def rect(sx,sy,sz,lx,lz,b):
+  line(sx,sy,sz,1,0,0,lx,b)
+  line(sx,sy,sz,0,0,1,lz,b)
+  line(sx,sy,sz+lz-1,1,0,0,lx,b)
+  line(sx+lx-1,sy,sz,0,0,1,lz,b)
+
+def square(sx,sy,sz,l,b):
+  rect(sx,sy,sz,l,l,b)
+
+def pyramid(sx,sy,sz,l,b):
+  if (l>0):
+    square(sx,sy,sz,l,b)
+    pyramid(sx+1,sy+1,sz+1,l-2,b)
+
+def circle(cx,cy,cz,r,b):
+  x=r
+  y=0
+  while (x>=y):
+    mc.setBlock(cx+x,cy,cz+y,b)
+    mc.setBlock(cx+x,cy,cz-y,b)
+    mc.setBlock(cx-x,cy,cz+y,b)
+    mc.setBlock(cx-x,cy,cz-y,b)
+    mc.setBlock(cx+y,cy,cz+x,b)
+    mc.setBlock(cx+y,cy,cz-x,b)
+    mc.setBlock(cx-y,cy,cz+x,b)
+    mc.setBlock(cx-y,cy,cz-x,b)
+    y=y+1
+    x=int(round(math.sqrt(r*r-y*y)))
+
+def cylinder(cx,cy,cz,r,h,b):
+  if (h>0):
+    circle(cx,cy,cz,r,b)
+    cylinder(cx,cy+1,cz,r,h-1,b)
+
+
+
+# eof
+
+

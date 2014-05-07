3b554c7388877505018cfd55bd8498defc5f0465
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2014-05-07 22:50:30 +0100

    processing CSV data

--- scala/csv/readCsv.scala
+++ scala/csv/readCsv.scala
@@ -11,6 +11,13 @@ object CSV {
     val csv=CSVReader.read(new FileReader("mytest.csv"),skipLines=1)
     println(csv)
     println(csv(1)(1))
+    val gender=csv map {x=>x(0)}
+    println(gender)
+    val height=csv map {x=>x(1)} map {_.toInt}
+    println(height)
+    val t=height.reduce(_+_)
+    val m=t.toDouble/height.length
+    println(m)
     println("goodbye")
   }
 

3e6945514bcff3e966c91ce6024a8f27910327df
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2014-11-10 21:41:08 +0000

    adding in the sbt launcher for the benefit of newbies...

--- scala/regression/src/main/scala/regression/Lm.scala
+++ scala/regression/src/main/scala/regression/Lm.scala
@@ -12,10 +12,18 @@ class Lm(X: DenseMatrix[Double], y: DenseVector[Double]) {
     beta.toString
   }
 
+  def thinQR(A: DenseMatrix[Double]): (DenseMatrix[Double], DenseMatrix[Double]) = {
+    import breeze.linalg
+    val n = A.rows
+    val p = A.cols
+    val linalg.qr.QR(_Q, _R) = linalg.qr(X)
+    (_Q(::, 0 until p), _R(0 until p, ::))
+  }
+
 }
 
- object Lm {
-    def apply(X: DenseMatrix[Double], y: DenseVector[Double]) = new Lm(X, y)
-  }
+object Lm {
+  def apply(X: DenseMatrix[Double], y: DenseVector[Double]) = new Lm(X, y)
+}
  
  

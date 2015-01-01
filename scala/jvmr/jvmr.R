d631671ec968c3c92654dc22c8a3ecccf5ed3acf
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2015-01-01 21:41:48 +0000

    tweak to sbtInit

--- scala/jvmr/jvmr.R
+++ scala/jvmr/jvmr.R
@@ -4,7 +4,8 @@ sbtInit<-function()
 {
   library(jvmr)
   system2("sbt","compile")
-  cpstr=system2("sbt","printClasspath",stdout=TRUE)[3]
+  cpstr=system2("sbt","printClasspath",stdout=TRUE)
+  cpstr=cpstr[length(cpstr)]
   cpsp=strsplit(cpstr,"!")[[1]]
   cp=cpsp[1:(length(cpsp)-1)]
   scalaInterpreter(cp)

258b5439b82ce523c9ca204da39c22873483f70d
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2015-01-22 22:04:01 +0000

    framian looks very promising as an R-like data table...

--- scala/framian-test/framian.scala
+++ scala/framian-test/framian.scala
@@ -6,9 +6,7 @@ Test of "framian"
 */
 
 import java.io.File
-import scala.annotation.tailrec
-import spire.implicits._
-import framian.Index
+import framian.{Index,Cols}
 import framian.csv.Csv
 
 object FramianTest {
@@ -16,7 +14,15 @@ object FramianTest {
   def main(args: Array[String]) = {
     println("Hello")
     val df=Csv.parseFile(new File("my-df.csv")).labeled.toFrame
-    println(df)
+    println(""+df.rows+" "+df.cols)
+    val df2=df.filter(Cols("Age").as[Int])( _ > 0 )
+    println(""+df2.rows+" "+df2.cols)
+    println(df2)
+    println(df2.get(Cols("OI").as[Double]).values)
+    println(df2.get(Cols("Age").as[Int]).values)
+    println(df2.get(Cols("Sex").as[String]).values)
+    println(""+df2.rows+" "+df2.cols)
+    println(df2.colIndex)
     println("Done")
   }
 

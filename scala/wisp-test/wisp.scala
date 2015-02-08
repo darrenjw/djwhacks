197e54aa55fe67006fd9d6b7084d25ced80c8c28
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2015-02-08 18:32:15 +0000

    extended wisp test

--- scala/wisp-test/wisp.scala
+++ scala/wisp-test/wisp.scala
@@ -11,10 +11,13 @@ object WispTest {
 
   def main(args: Array[String]) = {
     println("Hello")
+    help
     areaspline(List(1, 2, 3, 4, 5), List(4, 1, 3, 2, 6))
     areaspline(List(1, 2, 3, 4, 5), List(4, 1, 3, 2, 6))
     areaspline(List(1, 2, 3, 4, 5), List(4, 1, 3, 2, 6))
     delete
+    deleteAll
+    line((0 until 5).map(x => x -> x*x))
     stopServer
     println("Done")
   }

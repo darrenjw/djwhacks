aff94a4a66128cfd3455b115d5b8509961dc28fb
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2015-04-03 15:22:37 +0100

    R package loading function

--- r/package.R
+++ r/package.R
@@ -3,7 +3,6 @@
 package=function(somepackage)
 {
   cpackage <- as.character(substitute(somepackage))
-  print(cpackage)
   if(!require(cpackage,character.only=TRUE)){
     install.packages(cpackage)
     library(cpackage,character.only=TRUE)

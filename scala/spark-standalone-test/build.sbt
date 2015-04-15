952ad10a2b440d53e1d336f5daa79f95a982f9b3
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2015-04-15 22:56:09 +0100

    updated spark test to 1.3

--- scala/spark-standalone-test/build.sbt
+++ scala/spark-standalone-test/build.sbt
@@ -2,10 +2,7 @@ name := "Simple Project"
 
 version := "1.0"
 
-scalaVersion := "2.10.3"
+scalaVersion := "2.10.4"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "0.9.0-incubating"
-
-resolvers += "Akka Repository" at "http://repo.akka.io/releases/"
-
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.3.0"
 
--- scala/spark-standalone-test/src/main/scala/spark-standalone-test/SimpleApp.scala
+++ scala/spark-standalone-test/src/main/scala/spark-standalone-test/SimpleApp.scala
@@ -1,13 +1,13 @@
-/*** SimpleApp.scala ***/
-
+/* SimpleApp.scala */
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
 
 object SimpleApp {
   def main(args: Array[String]) {
     val logFile = "/etc/passwd" // Should be some file on your system
-    val sc = new SparkContext("local", "SimpleApp", "YOUR_SPARK_HOME",
-      List("target/scala-2.10/simple-project_2.10-1.0.jar"))
+    val conf = new SparkConf().setMaster("local").setAppName("Simple Application")
+    val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()

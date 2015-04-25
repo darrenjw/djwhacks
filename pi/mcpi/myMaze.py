ae4802452f55647dd87e3f6841adc982e5215e0a
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2015-04-25 14:36:18 +0000

    maze generation for the pi

---
+++ pi/mcpi/myMaze.py
@@ -0,0 +1,54 @@
+from myMinecraft import *
+import numpy
+from numpy.random import random_integers as rand
+ 
+def maze(width=81, height=81, complexity=.75, density=.75):
+    # Only odd shapes
+    shape = ((height // 2) * 2 + 1, (width // 2) * 2 + 1)
+    # Adjust complexity and density relative to maze size
+    complexity = int(complexity * (5 * (shape[0] + shape[1])))
+    density    = int(density * (shape[0] // 2 * shape[1] // 2))
+    # Build actual maze
+    Z = numpy.zeros(shape, dtype=bool)
+    # Fill borders
+    Z[0, :] = Z[-1, :] = 1
+    Z[:, 0] = Z[:, -1] = 1
+    # Make aisles
+    for i in range(density):
+        x, y = rand(0, shape[1] // 2) * 2, rand(0, shape[0] // 2) * 2
+        Z[y, x] = 1
+        for j in range(complexity):
+            neighbours = []
+            if x > 1:             neighbours.append((y, x - 2))
+            if x < shape[1] - 2:  neighbours.append((y, x + 2))
+            if y > 1:             neighbours.append((y - 2, x))
+            if y < shape[0] - 2:  neighbours.append((y + 2, x))
+            if len(neighbours):
+                y_,x_ = neighbours[rand(0, len(neighbours) - 1)]
+                if Z[y_, x_] == 0:
+                    Z[y_, x_] = 1
+                    Z[y_ + (y - y_) // 2, x_ + (x - x_) // 2] = 1
+                    x, y = x_, y_
+    return Z
+ 
+mymaze=maze(51,51).tolist()
+sx=54
+sy=55
+sz=-15
+mc.setBlock(sx,sy,sz,block.STONE)
+x=sx
+y=sy
+z=sz
+for row in mymaze:
+  x+=1
+  z=sz
+  for col in row:
+    z+=1
+    print x,sy,z
+    mc.setBlock(x,sy,z,block.STONE)
+    if col:
+      for y in range(6):
+        mc.setBlock(x,sy+y,z,block.STONE)
+
+
+

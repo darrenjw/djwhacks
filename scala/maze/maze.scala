b108b55936cd7ebb23fbbf48e1e1370112947646
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2015-04-26 09:08:49 +0100

    minimal spanning tree with random tie breaking

--- scala/maze/maze.scala
+++ scala/maze/maze.scala
@@ -4,6 +4,8 @@
 val width=10
 val height=8
 
+val rng = new scala.util.Random
+
 type Loc=(Int,Int)
 type Edge=(Loc,Loc)
 
@@ -20,15 +22,29 @@ def inrange(loc: Loc): Boolean = loc match {
  case (x,y) => ( (x>=0) & (y>=0) & (x<width) & (y<height) )
 }
 
-def extend(tree: Set[Edge]): Set[Edge] = {
+def extensions(tree: Set[Edge]): Set[Edge] = {
  val locs=tree.map{ e => Set(e._1,e._2) }.flatten
  val allEdges=locs.map{neighbours(_)}.flatten
  val newEdges=allEdges.filter { e => {
                  val l=to(e)
                  inrange(l) & !locs.contains(l) 
                  } }
- tree.union(newEdges)
+ newEdges
 }
+
+def pick[T](s: Set[T]):T = s.toList(rng.nextInt(s.size))
+
+@annotation.tailrec def genTree(currTree: Set[Edge]): Set[Edge] = {
+ val newEdges=extensions(currTree)
+ if (newEdges.size == 0) { currTree } else {
+  genTree(currTree + pick(newEdges))
+ }
+}
+
+// test code
+
+val test=Set( ((0,0),(0,1)) , ((0,1),(1,1)) )
+extensions(test)
 
 
 // eof

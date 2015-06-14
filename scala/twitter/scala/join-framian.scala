030d9e2b1197350fc7c6d19b66d2753371dbf7e9
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2015-06-14 20:53:55 +0100

    working, but bug in the header line output that is probably a bug in framian

--- scala/twitter/scala/join-framian.scala
+++ scala/twitter/scala/join-framian.scala
@@ -7,7 +7,7 @@ Joining twitter data using scala with framian
 
 import java.io.{ File, FileWriter, BufferedWriter }
 import framian.{ Frame, Index, Cols, Rows }
-import framian.csv.{ Csv, CsvFormat }
+import framian.csv.{ Csv, CsvFormat, CsvRow }
 
 object FramianJoin {
 
@@ -23,7 +23,7 @@ object FramianJoin {
     //println(mapper)
 
     println("Reading and parsing tweets")
-    //val csv=Csv.parseFile(new File("../data/first1k.csv")).labeled.toFrame
+    //val csv = Csv.parseFile(new File("../data/first1k.csv")).labeled.toFrame
     val csv = Csv.parseFile(new File("../data/tweets.csv")).labeled.toFrame
 
     println("Writing id list")
@@ -34,19 +34,20 @@ object FramianJoin {
 
     println("Augmenting frame with additional column")
     val augmented = csv.map(Cols("id").as[String], "screenname")(x => snMap.getOrElse(x, ""))
-    //println(csv)
+    println(csv.colIndex)
     println("Done. Now writing to file...")
+    println(augmented.colIndex)
     frame2csv(augmented, "augmented.csv")
     println("Done")
   }
 
+  // BUG in this regarding element ordering, but don't think it's in my code...
   def frame2csv[Row, Col](df: Frame[Row, Col], filename: String): Unit = {
-    val csv = Csv.fromFrame(CsvFormat.CSV)(df).toString
+    val csv = Csv.fromFrame(new CsvFormat(",", header = true))(df)
     val out = new BufferedWriter(new FileWriter(new File(filename)))
-    csv foreach { x => out.write(x.toString) }
+    out.write(csv.toString)
     out.close
   }
-
 
 }
 

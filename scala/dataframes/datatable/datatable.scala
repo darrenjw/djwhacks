092469b486c76107541f8455d89138d5e3bebd10
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2015-08-20 11:59:49 +0100

    working but needs tidying up

--- scala/dataframes/datatable/datatable.scala
+++ scala/dataframes/datatable/datatable.scala
@@ -5,21 +5,19 @@ Test of "scala-datatable" and "scala-csv"
 
 */
 
-import java.io.FileReader
+
+object DatatableTest {
+
+  def main(args: Array[String]) = {
+
+import java.io.{File,FileReader}
 import com.github.tototoshi.csv._
 import com.github.martincooper.datatable._
 import scala.annotation.tailrec
 import scala.util.Try
 
-
-object DatatableTest {
-
-  def main(args: Array[String]) = {
-    println("Hello")
-    println("First some messing around with a CSV file using scala-csv")
     val reader=CSVReader.open(new FileReader("../r/cars93.csv"))
     val all=reader.allWithHeaders()
-    println(all)
     reader.close()
 
 object StringCol
@@ -36,29 +34,24 @@ val dataCols=colSet map {pair => colTypes(pair._1) match {
                                         Try(x.toDouble).toOption.getOrElse(-99.0)})
   } 
 }
-val df=DataTable("Cars93",dataCols).get
 
-    val OI=all map {_("OI").toDouble}
-    println(OI)
-    val Age=all map {_("Age").toInt}
-    println(Age)
-    val Sex=all map {_("Sex")}
-    println(Sex)
-    println("Now some messing around with scala-datatable")
-    val OICol=new DataColumn[Double]("OI",OI)
-    val AgeCol=new DataColumn[Int]("Age",Age)
-    val SexCol=new DataColumn[String]("Sex",Sex)
-    val dtOpt=DataTable("MyDT",Seq(OICol,AgeCol,SexCol))
-    val dt=dtOpt.get
-    println(dt)
-    // filter the frame and convert the view back to a table
-    val dtF=dt.filter(row => row.as[Int]("Age") > 0 ).toDataTable
-    println(dtF)
-    println(dtF.length)
-    println(dtF.columns("Age").as[Int].data)
-    println(dtF.columns(0).name)
-    println(dtF.columns.as[Int]("Age").data)
-    println(dtF.columns.as[Int]("Age").data.map{_.toDouble})
+val df=DataTable("Cars93",dataCols).get
+df.foreach(r=>println(r.values))
+
+val df2=df.filter(row=>row.as[Double]("EngineSize")<=4.0).toDataTable
+df2.foreach(r=>println(r.values))
+
+val oldCol=df2.columns("Weight").as[Int]
+val newCol=new DataColumn[Double]("WeightKG",oldCol.data.map{_.toDouble*0.453592})
+val df3=df2.columns.add(newCol).get
+df3.foreach(r=>println(r.values))
+
+
+val writer = CSVWriter.open(new File("out.csv"))
+writer.writeRow(df3.columns.map{_.name})
+df3.foreach{r=>writer.writeRow(r.values)}
+writer.close()
+
     println("Done")
   }
 

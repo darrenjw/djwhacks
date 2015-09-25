dc64906fafd7fad94eb6c3caef2de3cea14a3c49
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2015-09-25 20:46:00 +0100

    generic "mean" function and method (the latter using implicits and the typeclass pattern)

---
+++ scala/typeclasses/mean-function.scala
@@ -0,0 +1,23 @@
+/* generic mean function */
+
+import spire.math._
+import spire.implicits._
+
+object Mean
+{
+
+  def mean[T: Numeric](it: Iterable[T]): Double = {
+    it.map(_.toDouble).sum / it.size
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(mean(List(1,2,3)))
+    println(mean(Vector(1.0,2.0,2.0,2.0)))
+  }
+
+}
+
+
+/* eof */
+
+
---
+++ scala/typeclasses/mean-method.scala
@@ -0,0 +1,35 @@
+/* generic mean method */
+
+
+import scala.language.higherKinds
+import spire.math._
+import spire.implicits._
+
+object MeanM
+{
+
+  trait Mean[T] {
+    def mean[T](it: Iterable[T]): Double
+  }
+  object Mean {
+    def mean[T: Numeric](it: Iterable[T]): Double = {
+      it.map(_.toDouble).sum / it.size
+    }
+  }
+  implicit class MeanIC[T: Numeric](it: Iterable[T]) {
+    def mean[T] = Mean.mean(it)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(List(1,2,3).mean)
+    println(Vector(1.0,2.0,2.0,2.0).mean)
+    import Mean.mean
+    println(mean(List(1,2)))
+  }
+
+}
+
+
+/* eof */
+
+

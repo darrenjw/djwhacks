a2f567cff4b0693e16308d4679943011883209ef
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2016-01-23 17:35:14 +0000

    some reminders for R/ESS

---
+++ r/Reminders.md
@@ -0,0 +1,10 @@
+# Reminders
+
+Reminders for R code and programming
+
+
+## Emacs/ESS
+
+* Reformat code: highlight region to reformat and then C-M-\ (ie. Ctrl-Alt-\)
+* Highlight whole buffer: C-x h
+
--- r/diffusion/dsrd2d.R
+++ r/diffusion/dsrd2d.R
@@ -18,10 +18,10 @@ y[round(D/2),round(D/2)]=20
 mvleft=function(m,i,j) {
     m[i,j]=m[i,j]-1
     if (j>1) {
-     m[i,j-1]=m[i,j-1]+1
+        m[i,j-1]=m[i,j-1]+1
     } else {
-     l=dim(m)[2]
-     m[i,l]=m[i,l]+1
+        l=dim(m)[2]
+        m[i,l]=m[i,l]+1
     }
     m
 }
@@ -29,9 +29,9 @@ mvleft=function(m,i,j) {
 mvright=function(m,i,j) {
     m[i,j]=m[i,j]-1
     if (j<dim(m)[2]) {
-     m[i,j+1]=m[i,j+1]+1
+        m[i,j+1]=m[i,j+1]+1
     } else {
-     m[i,1]=m[i,1]+1
+        m[i,1]=m[i,1]+1
     }
     m
 }
@@ -39,10 +39,10 @@ mvright=function(m,i,j) {
 mvup=function(m,i,j) {
     m[i,j]=m[i,j]-1
     if (i>1) {
-     m[i-1,j]=m[i-1,j]+1
+        m[i-1,j]=m[i-1,j]+1
     } else {
-     l=dim(m)[1]
-     m[l,j]=m[l,j]+1
+        l=dim(m)[1]
+        m[l,j]=m[l,j]+1
     }
     m
 }
@@ -50,90 +50,87 @@ mvup=function(m,i,j) {
 mvdown=function(m,i,j) {
     m[i,j]=m[i,j]-1
     if (i<dim(m)[1]) {
-     m[i+1,j]=m[i+1,j]+1
+        m[i+1,j]=m[i+1,j]+1
     } else {
-     m[1,j]=m[1,j]+1
+        m[1,j]=m[1,j]+1
     }
     m
 }
 
 
 diffuse=function(x,y,hd) {
-  r=sample(0:(length(as.vector(x))-1),1,prob=as.vector(hd))
-  i=1 + r %% D
-  j=1 + r %/% D
-  u=runif(1)
-  if (u<0.25) {
-   if (runif(1,0,x[i,j]+y[i,j])<=x[i,j]) {
-    x=mvleft(x,i,j)
-   } else {
-    y=mvleft(y,i,j)
-   }
-  } else if (u<0.5) {
-   if (runif(1,0,x[i,j]+y[i,j])<=x[i,j]) {
-    x=mvright(x,i,j)
-   } else {
-    y=mvright(y,i,j)
-   }
-  } else if (u<0.75) {
-   if (runif(1,0,x[i,j]+y[i,j])<=x[i,j]) {
-    x=mvup(x,i,j)
-   } else {
-    y=mvup(y,i,j)
-   }    
-  } else {
-   if (runif(1,0,x[i,j]+y[i,j])<=x[i,j]) {
-    x=mvdown(x,i,j)
-   } else {
-    y=mvdown(y,i,j)
-   }  
-  }
-  # if ((min(x)<0)|(min(y)<0)) stop("negative species count after diffusion step")
-  list(x,y)
+    r=sample(0:(length(as.vector(x))-1),1,prob=as.vector(hd))
+    i=1 + r %% D
+    j=1 + r %/% D
+    u=runif(1)
+    if (u<0.25) {
+        if (runif(1,0,x[i,j]+y[i,j])<=x[i,j]) {
+            x=mvleft(x,i,j)
+        } else {
+            y=mvleft(y,i,j)
+        }
+    } else if (u<0.5) {
+        if (runif(1,0,x[i,j]+y[i,j])<=x[i,j]) {
+            x=mvright(x,i,j)
+        } else {
+            y=mvright(y,i,j)
+        }
+    } else if (u<0.75) {
+        if (runif(1,0,x[i,j]+y[i,j])<=x[i,j]) {
+            x=mvup(x,i,j)
+        } else {
+            y=mvup(y,i,j)
+        }    
+    } else {
+        if (runif(1,0,x[i,j]+y[i,j])<=x[i,j]) {
+            x=mvdown(x,i,j)
+        } else {
+            y=mvdown(y,i,j)
+        }  
+    }
+    # if ((min(x)<0)|(min(y)<0)) stop("negative species count after diffusion step")
+    list(x,y)
 }
 
 react=function(x,y,h,hr) {
-  r=sample(0:(length(as.vector(x))-1),1,prob=as.vector(hr))
-  i=1 + r %% D
-  j=1 + r %/% D  
-  u=runif(1,0,h[[1]][i,j]+h[[2]][i,j]+h[[3]][i,j])
-  if (u<h[[1]][i,j]) {
-   x[i,j]=x[i,j]+1
-  } else if (u<h[[1]][i,j]+h[[2]][i,j]) {
-   x[i,j]=x[i,j]-1
-   y[i,j]=y[i,j]+1
-  } else {
-   y[i,j]=y[i,j]-1
-  }
-  # if ((min(x)<0)|(min(y)<0)) stop("negative species count after reaction step")
-  list(x,y)
+    r=sample(0:(length(as.vector(x))-1),1,prob=as.vector(hr))
+    i=1 + r %% D
+    j=1 + r %/% D  
+    u=runif(1,0,h[[1]][i,j]+h[[2]][i,j]+h[[3]][i,j])
+    if (u<h[[1]][i,j]) {
+        x[i,j]=x[i,j]+1
+    } else if (u<h[[1]][i,j]+h[[2]][i,j]) {
+        x[i,j]=x[i,j]-1
+        y[i,j]=y[i,j]+1
+    } else {
+        y[i,j]=y[i,j]-1
+    }
+                                        # if ((min(x)<0)|(min(y)<0)) stop("negative species count after reaction step")
+    list(x,y)
 }
 
 stepLV=function(x,y,dt) {
- t=0
- repeat {
-  #print(t)
-  #print(x)
-  #print(y)
-  h=list(th[1]*x,th[2]*x*y,th[3]*y)
-  hr=h[[1]]+h[[2]]+h[[3]]
-  hrs=sum(hr)
-  hd=dc*(x+y)*2 # assuming common diffusion coefficient for now
-  hds=sum(hd)
-  h0=hrs+hds
-  t=t+rexp(1,h0)
-  if (t>dt)
-      return(list(x,y))
-  if (runif(1,0,h0)<hds) {
-   l=diffuse(x,y,hd)
-   x=l[[1]]
-   y=l[[2]]
-  } else {
-   l=react(x,y,h,hr)
-   x=l[[1]]
-   y=l[[2]]
-  }
- }
+    t=0
+    repeat {
+        h=list(th[1]*x,th[2]*x*y,th[3]*y)
+        hr=h[[1]]+h[[2]]+h[[3]]
+        hrs=sum(hr)
+        hd=dc*(x+y)*2 # assuming common diffusion coefficient for now
+        hds=sum(hd)
+        h0=hrs+hds
+        t=t+rexp(1,h0)
+        if (t>dt)
+            return(list(x,y))
+        if (runif(1,0,h0)<hds) {
+            l=diffuse(x,y,hd)
+            x=l[[1]]
+            y=l[[2]]
+        } else {
+            l=react(x,y,h,hr)
+            x=l[[1]]
+            y=l[[2]]
+        }
+    }
 }
 
 
@@ -141,12 +138,12 @@ stepLV=function(x,y,dt) {
 
 op=par(mfrow=c(1,2))
 for (i in 1:N) {
-  message(paste(N-i," ",sep=""),appendLF=FALSE)
-  l=stepLV(x,y,dt)
-  x=l[[1]]
-  y=l[[2]]
-  image(x,main="x - prey")
-  image(y,main="y - predator")
+    message(paste(N-i," ",sep=""),appendLF=FALSE)
+    l=stepLV(x,y,dt)
+    x=l[[1]]
+    y=l[[2]]
+    image(x,main="x - prey")
+    image(y,main="y - predator")
 }
 
 

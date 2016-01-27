50597823d56c5fee80e8a6a0154cf8b1c8478efe
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2016-01-27 14:23:47 +0000

    fixed frame dropping problem when making movie from frames

--- scala/reaction-diffusion/make-movie.sh
+++ scala/reaction-diffusion/make-movie.sh
@@ -5,10 +5,11 @@ for name in img????.png
 do
   short="${name%.*}"
   echo $short
-  pngtopnm "$name" | pnmscale 20 | pnmtopng > "${short}-s.png"
+  #pngtopnm "$name" | pnmscale 20 | pnmtopng > "${short}-s.png"
+  convert "$name" -scale 1000x1000 -define png:color-type=2 "${short}-s.png"
 done
 
-avconv -r 4 -i img%04d-s.png -b:v 1000k movie.mp4
+avconv -r 4 -i img%04d-s.png -r 24 movie.mp4
 
 
 # eof

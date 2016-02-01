b97617ce6b38d65fc496c5ec97d3e5ac9f1c2f0d
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2016-02-01 22:38:05 +0000

    1d diffusion plot added

--- r/diffusion/dsdiff1d.R
+++ r/diffusion/dsdiff1d.R
@@ -1,34 +1,43 @@
-# dsdiff1d.R
-# Discrete stochastic diffusion on a 1d grid
+                                        # dsdiff1d.R
+                                        # Discrete stochastic diffusion on a 1d grid
 
 D=100 # num grid cells
-S=10000 # num reaction events
+S=30 # num time points
+dt=0.05
+N=S/dt
 
 state=rep(0,D)
-state[round(D/2)]=D
+state[round(D/2)]=100*D
 
-mat=matrix(0,nrow=S,ncol=D)
+mat=matrix(0,nrow=N,ncol=D)
 
-for (i in 1:S) {
- h=c(state,state) # first left reactions and then right reactions
- # h0=sum(h)
- r=sample(1:(2*D),1,prob=h)
- if (r<=D) {
-  # left
-  state[r]=state[r]-1
-  state[r-1]=state[r-1]+1
- }
- else {
-  # right
-  r=r-D
-  state[r]=state[r]-1
-  state[r+1]=state[r+1]+1
- }
- mat[i,]=state
+for (i in 1:N) {
+    t=0
+    repeat {
+        h=c(state,state) # first left reactions and then right reactions
+        h0=sum(h)
+        t=t+rexp(1,h0)
+        if (t>dt) break
+        r=sample(1:(2*D),1,prob=h)
+        if (r<=D) {
+                                        # left
+            state[r]=state[r]-1
+            state[r-1]=state[r-1]+1
+        }
+        else {
+                                        # right
+            r=r-D
+            state[r]=state[r]-1
+            state[r+1]=state[r+1]+1
+        }
+    }
+    mat[i,]=state
 }
 
-image(mat)
+png("dsdiff1d.png",800,600)
+image(mat[10:N,20:80],xlab="time",ylab="space",main="Discrete stochastic diffusion in 1d")
+dev.off()
 
-# eof
+                                        # eof
 
 

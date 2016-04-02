a60fc6458c19e0dc85a670ff57b5bc339c62b7b8
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2016-04-02 20:19:52 +0100

    allow an mcmc output file to be passed as a command line argument

--- scala/smfsb/mcmc-summ.R
+++ scala/smfsb/mcmc-summ.R
@@ -1,8 +1,13 @@
 # mcmc-summ.R
 # Do an mcmc summary
 
+args = commandArgs(TRUE)
+filename = "mcmc-out.csv"
+if (length(args)>0)
+	filename = args[1]
+message(filename)
 library(smfsb)
-mcmcSummary(read.csv("mcmc-out.csv"))
+mcmcSummary(read.csv(filename))
 
 # eof
 

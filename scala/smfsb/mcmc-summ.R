dd541c5dfc6c026dd8295c9d7a514ec3f0aff8af
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2016-04-03 19:49:26 +0100

    do log summaries and plots as well

--- scala/smfsb/mcmc-summ.R
+++ scala/smfsb/mcmc-summ.R
@@ -7,11 +7,15 @@ if (length(args)>0)
 	filename = args[1]
 message(filename)
 library(smfsb)
+
 mat=read.csv(filename)
-# regular summary and plots
 mcmcSummary(mat)
-# cross-correlation plot
 plot(mat,pch=19,cex=0.2)
+
+mat=log(mat)
+mcmcSummary(mat)
+plot(mat,pch=19,cex=0.2)
+
 
 # eof
 

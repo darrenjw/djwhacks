6019e9ab0579211a980a254a2cc5ff52ee8fb9a0
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2016-04-03 19:37:03 +0100

    added pair plots

--- scala/smfsb/mcmc-summ.R
+++ scala/smfsb/mcmc-summ.R
@@ -7,7 +7,11 @@ if (length(args)>0)
 	filename = args[1]
 message(filename)
 library(smfsb)
-mcmcSummary(read.csv(filename))
+mat=read.csv(filename)
+# regular summary and plots
+mcmcSummary(mat)
+# cross-correlation plot
+plot(mat,pch=19,cex=0.2)
 
 # eof
 

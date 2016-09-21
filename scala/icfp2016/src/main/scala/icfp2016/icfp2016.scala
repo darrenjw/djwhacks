b56baf39608b6f762db15b2534db4252e4035701
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2016-09-21 08:02:20 +0100

    bits and bobs

---
+++ scala/icfp2016/src/main/scala/icfp2016/icfp2016.scala
@@ -0,0 +1,23 @@
+/*
+icfp2016.scala
+
+
+
+ */
+
+object Origami {
+
+
+
+  def main(args: Array[String]): Unit = {
+    val fileName="problem01.txt"
+    val fileLines = io.Source.fromFile(fileName).getLines.toList
+
+    }
+
+
+}
+
+/* eof */
+
+

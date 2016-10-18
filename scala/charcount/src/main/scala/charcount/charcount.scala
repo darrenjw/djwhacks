ee60a0db727bf7716f21e7c6d20e5b7f1aa46aaa
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2016-10-18 20:42:31 +0100

    tweaks

--- scala/charcount/src/main/scala/charcount/charcount.scala
+++ scala/charcount/src/main/scala/charcount/charcount.scala
@@ -12,17 +12,27 @@ object CharCount {
 
   val wordFile = "/usr/share/dict/words"
   val wordsString = Source.fromFile(wordFile).getLines.map(_.trim).map(_.toLowerCase).mkString
-  val fullCharArray = wordsString.toCharArray.filter(_ >= ' ').filter(_!='\'')
+  val fullCharArray = wordsString.toCharArray.filter(_ >= ' ').filter(_ != '\'')
   val shortCharArray = fullCharArray.take(500)
 
+  // function for timing
+  def time[A](f: => A) = {
+    val s = System.nanoTime
+    val ret = f
+    println("time: " + (System.nanoTime - s) / 1e6 + "ms")
+    ret
+  }
 
   def main(args: Array[String]): Unit = {
     println("hi")
 
     println(shortCharArray.toList.mkString)
-    val fullCharArrayPar=fullCharArray.par
-    val freqMap=fullCharArray.map(c => Map(c->1L)).reduce(_ |+| _)
-    val freqMapP=fullCharArrayPar.map(c => Map(c->1L)).reduce(_ |+| _)
+
+    val fullCharArrayPar = fullCharArray.par
+
+    val freqMap = time { fullCharArray.map(c => Map(c -> 1L)).reduce(_ |+| _) }
+    val freqMapP = time { fullCharArrayPar.map(c => Map(c -> 1L)).reduce(_ |+| _) }
+
     println(freqMap)
     println(freqMapP)
 
@@ -30,6 +40,12 @@ object CharCount {
   }
 
 }
+
+/*
+
+sc.textFile("/usr/share/dict/words").map{_.toLowerCase}.flatMap{_.toCharArray}.map{(_,1)}.reduceByKey(_+_).collect()
+
+ */
 
 // eof
 

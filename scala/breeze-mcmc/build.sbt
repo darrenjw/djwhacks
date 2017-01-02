427f41e197352a30b8d7273996c763b142362494
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2017-01-02 17:30:03 +0000

    finding and fixing bugs in breeze's metropolis hastings code...

--- scala/breeze-mcmc/build.sbt
+++ scala/breeze-mcmc/build.sbt
@@ -5,10 +5,8 @@ version := "0.1"
 scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature")
 
 libraryDependencies  ++= Seq(
-            "org.scalacheck" %% "scalacheck" % "1.11.4" % "test",
-            "org.scalatest" %% "scalatest" % "2.1.7" % "test",
-            "org.scalanlp" %% "breeze" % "0.11.2",
-            "org.scalanlp" %% "breeze-natives" % "0.11.2"
+            "org.scalanlp" %% "breeze" % "latest.integration",
+            "org.scalanlp" %% "breeze-natives" % "latest.integration"
 )
 
 resolvers ++= Seq(
@@ -16,7 +14,7 @@ resolvers ++= Seq(
             "Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/"
 )
 
-scalaVersion := "2.11.6"
+scalaVersion := "2.12.1"
 
 
 
--- scala/breeze-mcmc/test.scala
+++ scala/breeze-mcmc/test.scala
@@ -7,21 +7,23 @@ object BreezeMcmc {
   import breeze.linalg._
 
   def main(args: Array[String]): Unit = {
-    val mc = metropolisHastings(1.0, (x: Double) =>
-      new Gaussian(x, 1.0))(x => Gamma(2.0, 1.0/3).logPdf(x))
+    val mc = metropolisHastings(1.0,(x: Double) =>
+    // val mc = metropolis(1.0,(x: Double) =>
+      Gaussian(x, 1.0+x))(x => Gamma(2.0, 1.0/3).logPdf(x))
     val it = mc.steps
-    val its = it.take(100000).toArray
+    val its = it.take(1000000).toArray
     val itsv = DenseVector[Double](its)
     println(min(itsv))
     println(sum(itsv) / itsv.length)
     println("This doesn't work! Bugs in MH Kernel implementation...")
+
     // Try Stuccio's stuff instead?!
     import breeze.stats.mcmc.ArbitraryMetropolisHastings
     val mh = ArbitraryMetropolisHastings(Gamma(2.0, 1.0/3).logPdf, 
-      (x: Double) => Gaussian(x, 1.0), 
-      (x: Double, xp: Double) => Gaussian(x, 1.0).logPdf(xp), 1.0)
+      (x: Double) => Gaussian(x, 1.0+x), 
+      (x: Double, xp: Double) => Gaussian(x, 1.0+x).logPdf(xp), 1.0)
     val sit = mh.samples
-    val its2=sit.take(100000).toArray
+    val its2=sit.take(1000000).toArray
     val itsv2=DenseVector[Double](its2)
     println(min(itsv2))
     println(sum(itsv2)/itsv2.length)

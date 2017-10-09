f314a44934a4528762dc55cacc543a2a6a05e9b7
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2017-10-09 22:31:03 +0100

    combining pages abstracted out

--- r/metagenomics/api-test.R
+++ r/metagenomics/api-test.R
@@ -5,41 +5,38 @@ library(jsonlite)
 
 baseURL = "https://www.ebi.ac.uk/metagenomics/api/v0.2"
 
+## abstract out page combination
+combinePages = function(url,ps=80) {
+    if (grepl('?',url,fixed=TRUE))
+        sep = "&"
+    else
+        sep = "?"
+    firstURL =paste(url,paste0("page_size=",ps),sep=sep)
+    first = fromJSON(firstURL)
+    lastPage = first$meta$pagination$pages
+    startURL = paste(url,paste0("page_size=",ps,"&page="),sep=sep)
+    urls = paste0(startURL,1:lastPage)
+    pages = lapply(urls,function(x) fromJSON(x)$data)
+    rbind_pages(pages)
+    }
+
 ## list of studies
-studiesURL = paste(baseURL,"studies?page_size=1000",sep="/")
-print(studiesURL)
-studies = fromJSON(studiesURL)
-studies
-studies$data$id
-studies$data$links
-
-## list of studies - query and assemble multiple pages...
-studiesURL = paste(baseURL,"studies",sep="/")
-studies = fromJSON(studiesURL)
-lastPage = studies$meta$pagination$pages
-startURL = paste(baseURL,"studies?page=",sep="/")
-studyURLs = paste(startURL,1:lastPage,sep="")
-studyPages = lapply(studyURLs,function(x) fromJSON(x)$data$id)
-studies = Reduce(c,studyPages)
-studies
-## N.B. For full "pages" can use "rbind_pages" to assemble...
-
+studies = combinePages(paste(baseURL,"studies",sep="/"))
+studies$id
 
 ## list of samples for a study
 myStudy = "SRP047083"
-samplesURL = paste(baseURL,paste("samples?page_size=1000&study_accession=",myStudy,sep=""),sep="/")
-samples = fromJSON(samplesURL)
+samplesURL = paste(baseURL,paste("samples?study_accession=",myStudy,sep=""),sep="/")
+samples = combinePages(samplesURL)
 samples
-samples$data$id
-samples$data$links
+samples$id
 
 ## list of runs for a sample
 mySample = "SRS711891"
 runsURL = paste(baseURL,paste("runs?page_size=1000&sample_accession=",mySample,sep=""),sep="/")
-runs = fromJSON(runsURL)
-runs
-runs$data$id
-runs$data$links
+runs = combinePages(runsURL)
+runs$id
+runs$links
 
 
 ## eof

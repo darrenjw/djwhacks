10391bb24869ec25a4ce735ecfc6467a1260f507
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2017-10-10 14:10:59 +0100

    attempting to get started with rjsonapi...

--- r/metagenomics/rjsonapi-test.R
+++ r/metagenomics/rjsonapi-test.R
@@ -3,6 +3,18 @@
 
 library(rjsonapi)
 
+baseURL = "https://www.ebi.ac.uk"
+APIVersion = "metagenomics/api/v0.2"
+conn = jsonapi_connect(baseURL,APIVersion)
+conn
+conn$version
+conn$url
+conn$base_url()
+conn$status()
+conn$routes()
+
+
+
 
 
 

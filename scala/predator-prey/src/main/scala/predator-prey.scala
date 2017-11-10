40053a9a9c4275c6ad034ec8a0bd4c6c1425dc3c
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2017-11-10 19:20:36 +0000

    downgrade sbt version

--- scala/predator-prey/src/main/scala/predator-prey.scala
+++ scala/predator-prey/src/main/scala/predator-prey.scala
@@ -1,16 +1,15 @@
 /*
-Stub for scala-glm code
+predator-prey.scala
+Main runner script
 */
 
-object Stub {
+object PredatorPrey {
 
-  import scalaglm.Pca
-  import breeze.linalg._
 
   def main(args: Array[String]): Unit = {
-    val X = DenseMatrix((1.0,1.5),(1.5,2.0),(2.0,1.5))
-    val pca = Pca(X, List("V1","V2"))
-    pca.summary
+    println("Hi")
+
+    println("Bye")
   }
 
 }

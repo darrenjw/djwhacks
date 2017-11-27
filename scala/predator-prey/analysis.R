738ae20d9b12adf2eda14311db770527327b472f
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2017-11-27 19:59:00 +0000

    analysis

--- scala/predator-prey/analysis.R
+++ scala/predator-prey/analysis.R
@@ -14,8 +14,8 @@ plot(df$Day,df$AOB,type="l",col=3,ylim=c(10,8.0e07),main="AOB")
 par(op)
 
 ## read in the MCMC output
-out = read.csv("LvPmmh.csv")
-#out = read.csv("LvPmmh-t15.csv.gz")
+#out = read.csv("LvPmmh.csv")
+out = read.csv("LvPmmh-t30.csv.gz")
 library(smfsb)
 mcmcSummary(out[,c(1:4,9)])
 mcmcSummary(log(out[,1:8]))

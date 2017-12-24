180d6e49b58e3b57a7a8cd6f4a57b43199fffdf5
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2017-12-24 22:45:38 +0000

    a slightly more functional approach...

--- scala/scalajs-test/src/main/scala/scalajs-test.scala
+++ scala/scalajs-test/src/main/scala/scalajs-test.scala
@@ -1,41 +1,62 @@
 import scala.scalajs.js.annotation.{JSExport,JSExportTopLevel}
 import org.scalajs.dom
 import org.scalajs.dom.html
-import scala.util.Random
 
 @JSExportTopLevel("JsApp")
 object ScalaJsTest {
 
+  val cSize = 512
+  val size = 20
+
+  case class Square(x: Int, y: Int, vx: Int, vy: Int, col: String) {
+
+    def render(ctx: dom.CanvasRenderingContext2D): Unit = {
+      ctx.fillStyle = col
+      ctx.fillRect(x, y, size, size)
+    }
+
+  }
+
+  val squares = List(
+    Square(0,0,4,3,"blue"),
+    Square(200,0,2,-1,"red"),
+    Square(100,50,1,2,"green")
+  )
+
+  def nextSquare(sq: Square): Square = {
+    val can = sq.copy(x=sq.x+sq.vx,y=sq.y+sq.vy)
+    if (can.x + size >= cSize) sq.copy(vx = -sq.vx)
+    else if (can.y + size >= cSize) sq.copy(vy = -sq.vy)
+    else if (can.x <= 0) sq.copy(vx = -sq.vx)
+    else if (can.y <= 0) sq.copy(vy = -sq.vy)
+    else can
+  }
+
+  def squaresS = Stream.iterate(squares)(_ map nextSquare)
+
   @JSExport
   def main(canvas: html.Canvas): Unit = {
+
     val ctx = canvas.getContext("2d")
                     .asInstanceOf[dom.CanvasRenderingContext2D]
-
-    val cSize = 512
 
     def clear() = {
       ctx.fillStyle = "lightgrey"
       ctx.fillRect(0, 0, cSize, cSize)
     }
 
-    var x = 0
-    var y = 0
-    val size = 20
-    var vx = 4
-    var vy = 3
+    var sqrs = squaresS
 
     def run = {
-      x=x+vx
-      y=y+vy
-      if (x+size >= cSize) vx = -vx
-      if (y+size >= cSize) vy = -vy
-      if (x <= 0) vx = -vx
-      if (y <= 0) vy = -vy
+      val sq = sqrs.head
+      sqrs = sqrs.tail
       clear
-      ctx.fillStyle = "black"
-      ctx.fillRect(x, y, size, size)
+      sq map (_.render(ctx))
     }
 
-    dom.window.setInterval(() => run, 50)
+    dom.window.setInterval(() => run, 10)
+
   }
+
 }
+

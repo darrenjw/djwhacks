5b20622a09caed1d3982621d15da4526f3d1c743
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-04-11 17:40:59 +0100

    tweak to change program - input and output

--- haskell/change/change.hs
+++ haskell/change/change.hs
@@ -2,19 +2,43 @@
 change.hs
 
 First stab at a "count change" program in Haskell...
+How many different ways can the amount be made using the given coins
+
+ghci
+:load change.hs
+change 12
+
+runhaskell change.hs
+
+ghc -o change change.hs
+./change
+
 
 -}
 
-
-change :: Int -> [Int] -> Int
-change = \x c -> if (x==0) then 1 else
+changeCoins :: Int -> [Int] -> Int
+changeCoins = \x c -> if (x==0) then 1 else
   if (length c == 0) then 0 
     else let first = head c
              rest = tail c
-         in if (first > x) then change x rest else
-      (change (x - first) c) + (change x rest) 
+         in if (first > x) then changeCoins x rest else
+      (changeCoins (x - first) c) + (changeCoins x rest) 
 
-main = print (change (read "12" :: Int) [50,20,10,5,2,1])
+coins = [200,100,50,20,10,5,2,1]
+
+change :: Int -> Int
+change = \x -> changeCoins x coins
+
+main = do
+  putStrLn "Enter an amount of money (in pence)"
+  amountStr <- getLine
+  putStrLn ("Number of ways " ++ amountStr ++ "p could be made is:")
+  let amount = (read amountStr :: Int)
+  --putStrLn (show (change amount))
+  print (change amount)
+
+  
+  
 
 -- eof
 

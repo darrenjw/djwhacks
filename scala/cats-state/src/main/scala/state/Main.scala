b0a4b04cb6cd0ffc498b2b418d0b4cc7ab9d441e
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-04-12 13:11:48 +0100

    pure random numbers with the State monad

--- scala/cats-state/src/main/scala/state/Main.scala
+++ scala/cats-state/src/main/scala/state/Main.scala
@@ -1,8 +1,42 @@
+/*
+Main.scala
+
+Messing around with the State monad in Cats
+
+*/
+
+
 package state
 
-import cats.instances.string._
-import cats.syntax.semigroup._
+import cats.data.State
+
+final case class Seed(long: Long) {
+  def next = Seed(long * 6364136223846793005L + 1442695040888963407L)
+}
 
 object Main extends App {
-  println("Hello " |+| "Cats!")
+
+  val nextLong: State[Seed,Long] = State((s: Seed) => (s.next,s.long))
+  val nextBoolean = nextLong map (l => l >= 0L)
+  val nextDie = nextLong map (l => (l.abs % 6).toInt + 1)
+
+  val roll2Dice = for {
+    roll1 <- nextDie
+    roll2 <- nextDie
+  } yield (roll1, roll2)
+
+  println("hello")
+  val s = Seed(2L)
+  println(s)
+  println(s.next)
+  println(nextLong.run(s).value)
+  println(nextBoolean.run(s).value)
+  println(nextBoolean.runA(s).value)
+  println(nextDie.runA(s).value)
+  println(roll2Dice.runA(s).value)
+  println("bye")
 }
+
+
+// eof
+

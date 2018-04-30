d7351983ad396b1d0be2f8c60e4f1c40f8f4627f
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-04-30 21:58:26 +0100

    added a single (uncoupled) chain

--- scala/unbiased-mcmc/src/main/scala/unbiased-mcmc.scala
+++ scala/unbiased-mcmc/src/main/scala/unbiased-mcmc.scala
@@ -5,17 +5,16 @@ Messing around with coupling and debiasing of MCMC chains
 
 */
 
-object UnbiasedMcmc {
+import breeze.stats.distributions._
 
-  import breeze.stats.distributions._
+object UnbiasedMcmc {
 
   // Monadic coupling of two continuous distributions
   def couple[T](p: ContinuousDistr[T], q: ContinuousDistr[T]): Rand[(T,T)] = {
     def ys: Rand[T] = for {
       y <- q
       w <- Uniform(0, q.pdf(y))
-      ayd = if (w > p.pdf(y)) Rand.always(y) else ys
-      ay <- ayd
+      ay <- if (w > p.pdf(y)) Rand.always(y) else ys
     } yield ay
     for {
       x <- p
@@ -25,9 +24,7 @@ object UnbiasedMcmc {
   }
 
 
-
-  def main(args: Array[String]): Unit = {
-
+  def couplingTest: Unit = {
     // val c = couple(Gamma(10,5), Gamma(20,2.5))
     val c = couple(Gaussian(5,2), Gaussian(4,3))
     val cs = c.sample(1000)
@@ -42,7 +39,30 @@ object UnbiasedMcmc {
     p1 += hist(x)
     val p2 = fig.subplot(1,3,2)
     p2 += hist(y)
+  }
 
+  def metropTest: Unit = {
+    val chain = MarkovChain.metropolis(0.0,
+      (x: Double) => Uniform(x-0.5, x+0.5))(
+      x => Gaussian(0.0, 1.0).logPdf(x)).
+      steps.
+      drop(1).
+      take(10000).
+      toArray
+
+    import breeze.plot._
+    import breeze.linalg._
+    val fig = Figure("Metropolis chain")
+    val p0 = fig.subplot(0)
+    p0 += plot(linspace(1,chain.length,chain.length),chain)
+  }
+
+  
+
+
+  def main(args: Array[String]): Unit = {
+    couplingTest
+    metropTest
   }
 
 }

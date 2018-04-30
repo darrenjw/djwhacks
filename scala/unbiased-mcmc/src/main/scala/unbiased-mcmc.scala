3bb9165a9dd91974a2c339a101d70c7acfb4bece
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-04-30 23:15:26 +0100

    simple coupling of chains now working

--- scala/unbiased-mcmc/src/main/scala/unbiased-mcmc.scala
+++ scala/unbiased-mcmc/src/main/scala/unbiased-mcmc.scala
@@ -16,18 +16,32 @@ object UnbiasedMcmc {
       w <- Uniform(0, q.pdf(y))
       ay <- if (w > p.pdf(y)) Rand.always(y) else ys
     } yield ay
-    for {
+    val pair = for {
       x <- p
       w <- Uniform(0, p.pdf(x))
-      y <- ys
-    } yield if (w < q.pdf(x)) (x,x) else (x,y)
+    } yield (w <= q.pdf(x), x)
+    pair flatMap {
+      case (b,x) => if (b) Rand.always((x,x)) else (ys map (y => (x,y)))
+    }
   }
+
+  def coupledKernel[T](q: T => ContinuousDistr[T])(logPi: T =>
+      Double)(x: (T,T)): Rand[(T,T)] = for {
+    p <- couple(q(x._1), q(x._2))
+    //p <- for (q1 <- q(x._1); q2 <- q(x._2)) yield (q1,q2)
+    u <- Uniform(0,1)
+    n1 = if (math.log(u) < logPi(p._1) - logPi(x._1)) p._1 else x._1
+    n2 = if (math.log(u) < logPi(p._2) - logPi(x._2)) p._2 else x._2
+  } yield (n1,n2)
 
 
   def couplingTest: Unit = {
-    // val c = couple(Gamma(10,5), Gamma(20,2.5))
-    val c = couple(Gaussian(5,2), Gaussian(4,3))
-    val cs = c.sample(1000)
+    //val c = couple(Gamma(10,0.1), Gamma(10,0.1))
+    //val c = couple(Gamma(10,0.1), Gamma(20,0.1))
+    //val c = couple(Gaussian(5,2), Gaussian(4,3))
+    val c = couple(Gaussian(0,1),Gaussian(1,1))
+    //val c = couple(Uniform(0,2),Uniform(1,4))
+    val cs = c.sample(10000)
     val x = cs map (_._1)
     val y = cs map (_._2)
 
@@ -57,12 +71,27 @@ object UnbiasedMcmc {
     p0 += plot(linspace(1,chain.length,chain.length),chain)
   }
 
-  
+  def coupledMetropTest: Unit = {
+    val chain = MarkovChain((10.0,-10.0))(coupledKernel((x: Double) => Uniform(x-0.5, x+0.5))(x => Gaussian(0.0,1.0).logPdf(x))).
+      steps.
+      drop(1).
+      take(1000).
+      toArray
+    val x = chain map (_._1)
+    val y = chain map (_._2)
 
+    import breeze.plot._
+    import breeze.linalg._
+    val fig = Figure("Metropolis chain")
+    val p0 = fig.subplot(0)
+    p0 += plot(linspace(1,x.length,x.length),x)
+    p0 += plot(linspace(1,y.length,y.length),y)
+  }
 
   def main(args: Array[String]): Unit = {
-    couplingTest
-    metropTest
+    //couplingTest
+    //metropTest
+    coupledMetropTest
   }
 
 }

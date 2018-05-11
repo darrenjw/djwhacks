48fd41481fa5f3b367d5928c8cb072e79a9aa9c1
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-05-11 18:58:45 +0100

    evil plot working with scala-view

---
+++ scala/evilplot-test/build.sbt
@@ -0,0 +1,23 @@
+name := "evilplot-test"
+
+version := "0.1"
+
+scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature")
+
+libraryDependencies  ++= Seq(
+	"com.cibo" %% "evilplot" % "0.2.0",
+	"com.github.darrenjw" %% "scala-view" % "0.6-SNAPSHOT"
+)
+
+resolvers ++= Seq(
+            "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
+            "Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/",
+	Resolver.bintrayRepo("cibotech", "public")
+)
+
+scalaVersion := "2.12.4"
+
+fork := true
+
+
+
---
+++ scala/evilplot-test/src/main/scala/evilplot.scala
@@ -0,0 +1,80 @@
+/*
+evilplot.scala
+
+Test of the EvilPlot library and using it in conjunction with scala-view
+
+ */
+
+object EvilPlotTest {
+
+  import com.cibo.evilplot.plot._
+  import com.cibo.evilplot.colors._
+  import com.cibo.evilplot.plot.aesthetics.DefaultTheme._
+  import com.cibo.evilplot.numeric.Point
+  import java.awt.image.BufferedImage
+  import java.awt.Image.SCALE_SMOOTH
+  import scalafx.embed.swing.SwingFXUtils
+  import scalaview.Utils._
+
+  def simpleScatter: Unit = {
+    val data = Seq.tabulate(100) { i =>
+      Point(i.toDouble, scala.util.Random.nextDouble())
+    }
+    val plot = ScatterPlot(data)
+      .xAxis()
+      .yAxis()
+      .frame()
+      .xLabel("x")
+      .yLabel("y")
+      .render()
+    val plotImage = plot.asBufferedImage
+    //scalaview.SwingImageViewer(biResize(plotImage,1000,800,SCALE_SMOOTH))
+    scalaview.SfxImageViewer(biResize(plotImage,1000,800,SCALE_SMOOTH))
+  }
+
+  def simpleLineGraph: Unit = {
+    val data = Seq.tabulate(100) { i =>
+      Point(i.toDouble, scala.util.Random.nextDouble())
+    }
+    val plot = LinePlot.series(data, "Line graph", HSL(210, 100, 56))
+      .xAxis()
+      .yAxis()
+      .frame()
+      .xLabel("x")
+      .yLabel("y")
+      .render()
+    scalaview.SfxImageViewer(biResize(plot.asBufferedImage,1000,800,SCALE_SMOOTH))
+  }
+
+  def simplePlotStream: Unit = {
+    val data = Seq.tabulate(100) { i =>
+      Point(i.toDouble, scala.util.Random.nextDouble())
+    }
+    val dataStream = data.toStream
+    val cumulStream = dataStream.scanLeft(Nil: List[Point])((l,p) => p :: l).drop(1)
+    def dataToImage(data: List[Point]): BufferedImage = {
+      val plot = LinePlot.series(data, "Line graph", HSL(210, 100, 56))
+        .xAxis()
+        .yAxis()
+        .frame()
+        .xLabel("x")
+        .yLabel("y")
+        .render()
+      plot.asBufferedImage
+    }
+    val plotStream = cumulStream map (d => biResize(dataToImage(d),1000,800,SCALE_SMOOTH))
+    scalaview.SfxImageViewer.bi(plotStream, 100000, autoStart=true)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("Hi")
+    //simpleScatter
+    //simpleLineGraph
+    simplePlotStream
+    println("Bye")
+  }
+
+}
+
+// eof
+

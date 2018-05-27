05840d0cf0f9a0ee09928186944994997f94c63d
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-05-27 18:28:23 +0100

    1d cle now working

---
+++ r/diffusion/cle1d.R
@@ -0,0 +1,104 @@
+## cle1d.R
+
+library(smfsb)
+
+StepCLE1D <- function(N, d, dt=0.01) {
+    S = t(N$Post-N$Pre)
+    v = ncol(S)
+    u = nrow(S)
+    sdt = sqrt(dt)
+    forward <- function(m) cbind(m[,2:ncol(m)],m[,1])
+    back <- function(m) {
+        n = ncol(m)
+        cbind(m[,n],m[,1:(n-1)])
+    }
+    laplacian <- function(m) forward(m) + back(m) - 2*m
+    rectify <- function(m) {
+        m[m<0] = 0 # absorb at 0
+        m
+    }
+    diffuse <- function(m) {
+        n = ncol(m)
+        noise = matrix(rnorm(n*u,0,sdt),nrow=u)
+        m = m + d*laplacian(m)*dt + sqrt(d)*(
+            sqrt(m+forward(m))*noise -
+            sqrt(m+back(m))*back(noise))
+        m = rectify(m)
+        m
+    }
+    return(function(x0, t0, deltat, ...) {
+        x = x0
+        t = t0
+        n = ncol(x0)
+        termt = t0 + deltat
+        repeat {
+            x = diffuse(x)
+            hr = apply(x,2,function(x){N$h(x, t, ...)})
+            dwt = matrix(rnorm(n*v,0,sdt),nrow=v)
+            x = x + S %*% (hr*dt + sqrt(hr)*dwt)
+            x = rectify(x)
+            t = t + dt
+            if (t > termt)
+                return(x)
+        }
+    })
+}
+
+
+
+## Test
+
+LVTest <- function() {
+    N=200
+    T=50
+    data(spnModels)
+    x0=matrix(0,nrow=2,ncol=N)
+    rownames(x0)=c("x1","x2")
+    x0[,round(N/2)]=LV$M
+    stepLV1D = StepCLE1D(LV,c(0.6,0.6))
+    xx = simTs1D(x0,0,T,0.2,stepLV1D,verb=TRUE)
+    op=par(mfrow=c(1,2))
+    image(xx[1,,],main="Prey",xlab="Space",ylab="Time")
+    image(xx[2,,],main="Predator",xlab="Space",ylab="Time")
+    par(op)
+    library(grid)
+    r = xx[2,,]/max(xx[2,,])
+    g = xx[2,,]*0
+    b = xx[1,,]/max(xx[1,,])
+    col = rgb(r,g,b)
+    dim(col)=dim(r)
+    grid.newpage()
+    grid.raster(col,interpolate=FALSE)    
+}
+
+SIRTest <- function() {
+    N=200
+    T=10
+    data(spnModels)
+    x0=matrix(0,nrow=3,ncol=N)
+    rownames(x0)=c("S","I","R")
+    x0[1,]=100
+    x0[2,round(N/2)]=20
+    stepSIR1D = StepCLE1D(SIR,c(1,0.5,0))
+    xx = simTs1D(x0,0,T,0.05,stepSIR1D,verb=TRUE)
+    op=par(mfrow=c(1,3))
+    image(xx[1,,],main="S",xlab="Space",ylab="Time")
+    image(xx[2,,],main="I",xlab="Space",ylab="Time")
+    image(xx[3,,],main="R",xlab="Space",ylab="Time")
+    par(op)
+    library(grid)
+    r = xx[1,,]/max(xx[1,,])
+    g = xx[2,,]/max(xx[2,,])
+    b = xx[3,,]/max(xx[3,,])
+    col = rgb(r,g,b)
+    dim(col)=dim(r)
+    grid.newpage()
+    grid.raster(col,interpolate=FALSE)    
+}
+
+
+LVTest()
+##SIRTest()
+
+
+## eof
--- r/diffusion/gillespie2d.R
+++ r/diffusion/gillespie2d.R
@@ -2,102 +2,6 @@
 
 library(smfsb)
 
-StepGillespie2D <- function(N,d) {
-    S = t(N$Post - N$Pre)
-    v = ncol(S)
-    u = nrow(S)
-    return(function(x0, t0, deltat, ...) {
-        t = t0
-        x = x0
-        m = dim(x)[2]
-        n = dim(x)[3]
-        termt = t + deltat
-        repeat {
-            hr = apply(x,c(2,3),function(x){N$h(x, t, ...)})
-            hrs = apply(hr,c(2,3),sum)
-            hrss = sum(hrs)
-            hd = x * (d*4)
-            hds = apply(hd,c(2,3),sum)
-            hdss = sum(hds)
-            h0 = hrss + hdss
-            if (h0 < 1e-10)
-                t = 1e+99
-            else if (h0 > 1e+07) {
-                t = 1e+99
-                warning("Hazard too big - terminating!")
-            } else
-                t = t + rexp(1, h0)
-            if (t > termt) return(x)
-            if (runif(1,0,h0) < hdss) {
-                ## diffuse
-                r = sample(0:(n*m-1),1,prob=as.vector(hds)) # pick a box
-                i = 1 + r %% m
-                j = 1 + r %/% m
-                k = sample(u, 1, prob=hd[,i,j]) # pick a species
-                x[k,i,j] = x[k,i,j]-1 # decrement chosen box
-                un = runif(1)
-                if (un < 0.25) {
-                    ## down
-                    if (j>1)
-                        x[k,i,j-1] = x[k,i,j-1]+1
-                    else
-                        x[k,i,n] = x[k,i,n]+1
-                } else if (un < 0.5) {
-                    ## up
-                    if (j<n)
-                        x[k,i,j+1] = x[k,i,j+1]+1
-                    else
-                        x[k,i,1] = x[k,i,1]+1
-                } else if (un < 0.75) {
-                    ## left
-                    if (i>1)
-                        x[k,i-1,j] = x[k,i-1,j]+1
-                    else
-                        x[k,m,j] = x[k,m,j]+1
-                } else {
-                    ## right
-                    if (i<m)
-                        x[k,i+1,j] = x[k,i+1,j]+1
-                    else
-                        x[k,1,j] = x[k,1,j]+1
-                }
-            } else {
-                ## react
-                r = sample(0:(n*m-1), 1, prob=as.vector(hrs)) # pick a box
-                i = 1 + r %% m
-                j = 1 + r %/% m
-                k = sample(v, 1, prob=hr[,i,j]) # pick a reaction
-                x[,i,j] = x[,i,j] + S[,k]
-            }
-        }
-    })
-}
-
-simTs2D <- function (x0, t0 = 0, tt = 100, dt = 0.1, stepFun, verb=FALSE, ...) 
-{
-    N = (tt - t0)%/%dt + 1
-    u = dim(x0)[1]
-    m = dim(x0)[2]
-    n = dim(x0)[3]
-    arr = array(0,c(u,m,n,N))
-    x = x0
-    t = t0
-    arr[,,,1] = x
-    if (verb) cat("Steps",dimnames(x)[[1]],"\n")
-    for (i in 2:N) {
-        if (verb) cat(N-i,apply(x,1,sum),"\n")
-        if (verb) image(x[1,,])
-        t = t + dt
-        x = stepFun(x, t, dt, ...)
-        arr[,,,i] = x
-    }
-    arr
-}
-
-
-
-## test...
-
 LVTest <- function() {
     m=40
     n=30
@@ -153,8 +57,8 @@ SIRTest <- function() {
 }
 
 
-LVTest()
-##SIRTest()
+##LVTest()
+SIRTest()
 
 
 ## eof

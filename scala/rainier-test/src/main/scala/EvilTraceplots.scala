c2ff9c97e81c3bb6284321c55373a06a67aa7c05
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-05-31 16:35:07 +0100

    Some nice diagnostic plots

--- scala/rainier-test/src/main/scala/EvilTraceplots.scala
+++ scala/rainier-test/src/main/scala/EvilTraceplots.scala
@@ -15,27 +15,59 @@ object EvilTraceplots {
   import com.cibo.evilplot.numeric.Point
 
 
-  def traces(out: Seq[Map[String,Double]], truth: Map[String,Double] = Map()): List[List[Plot]] = {
+  /**
+    * Autocorrelation function
+    * 
+    * @param x Input data
+    * @param lm Maximum lag required
+    */
+  def acf(x: Seq[Double], lm: Int): List[Double] = {
+    val m = x.sum/x.length
+    val xx = x map (_ - m)
+    val v = (xx map (xi => xi*xi)).reduce(_+_)/x.length
+      ((0 until lm) map ((k: Int) =>
+        (((xx zip (xx drop k)) map (p => p._1*p._2)) reduce (_+_) )/(v*xx.length)
+      )
+      ).toList
+  }
+
+  /**
+    * Generate marginal diagnostic plots for MCMC output in the form `Seq[Map[String,Double]]`, where the map is from variable name to sampled value.
+    * 
+    * @param out The MCMC output
+    * @param truth Optional "true" values to be overlaid in the case of synthetic data
+    * @param lagMax Maximum autocorrelation to be computed for the ACF plots
+    * @param numBars Number of bars to use in the histograms
+    */
+  def traces(out: Seq[Map[String,Double]], truth: Map[String,Double] = Map(), lagMax: Int = 40, numBars: Int = 50): List[List[Plot]] = {
     val keys = out.head.keys.toList.sorted
     keys map (k => {
       val data = out.map(_(k)).zipWithIndex.map(p => Point(p._2,p._1))
       val trace = LinePlot.series(data, "Line graph", HSL(210, 100, 56)).
         xAxis().yAxis().frame().
         xLabel("Iteration").yLabel(k).title("Trace plot")
-      val hist = Histogram(out.map(_(k)),30).xAxis().yAxis().frame().
+      val aplot = BarChart(acf(out.map(_(k)),lagMax)).xAxis().yAxis().frame()
+      val hist = Histogram(out.map(_(k)),numBars).xAxis().yAxis().frame().
         xLabel(k).yLabel("Frequency")
         truth.get(k) match {
-          case None => List(trace, hist)
-          case Some(v) => List(trace.hline(v),hist.vline(v))
+          case None => List(trace, aplot, hist)
+          case Some(v) => List(trace.hline(v), aplot, hist.vline(v))
         }
     })
   }
 
-  def pairs(out: Seq[Map[String,Double]], truth: Map[String,Double] = Map()): List[List[Plot]] = {
+  /**
+    * Generate pairs of bivariate marginal plots.
+    * 
+    * @param out The MCMC output
+    * @param truth Optional true values to be overlaid in the case of synthetic data
+    * @param numBars Number of bars to use in the diagonal histograms
+    */
+  def pairs(out: Seq[Map[String,Double]], truth: Map[String,Double] = Map(), numBars: Int = 30): List[List[Plot]] = {
     val keys = out.head.keys.toList.sorted
     keys map (k1 => keys map (k2 => {
       if (k1 == k2) {
-        val hist = Histogram(out.map(_(k1)),30).xAxis().yAxis().frame().
+        val hist = Histogram(out.map(_(k1)),numBars).xAxis().yAxis().frame().
           xLabel(k1).yLabel("Frequency")
         truth.get(k1) match {
           case None => hist

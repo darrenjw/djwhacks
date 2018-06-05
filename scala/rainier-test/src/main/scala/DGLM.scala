d04984c7d9c4f29da9015796c84cce344c63ab77
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-06-05 20:45:33 +0100

    working on a DGLM example

--- scala/rainier-test/src/main/scala/DGLM.scala
+++ scala/rainier-test/src/main/scala/DGLM.scala
@@ -15,7 +15,7 @@ object DGLM {
 
   implicit val rng = RNG.default
 
-  // TODO: Spectacularly bad way of generating Poisson RVs...
+  // TODO: *Spectacularly* bad way of generating Poisson RVs...
   def genPois(lambda: Double): Int =
     RandomVariable(Poisson(lambda).generator).sample().head
 
@@ -23,7 +23,7 @@ object DGLM {
 
     // first simulate some data from a DGLM model
     val r = new scala.util.Random
-    val n = 250 // time points
+    val n = 50 // time points
     val mu = 1.0 // AR(1) mean
     val a = 0.95 // auto-regressive parameter
     val sig = 0.5 // AR(1) SD
@@ -33,40 +33,43 @@ object DGLM {
       .toVector
     val obs = state map (s => genPois(math.exp(s)))
 
+    case class Static(mu: Real, a: Real, sig: Real)
+
     // build and fit model
     val prior = for {
       mu <- Normal(0, 100).param
       a <- Normal(1, 0.2).param
       sig <- LogNormal(0, 10).param
       sp <- Normal(0, 100).param
-    } yield Map("mu" -> mu, "a" -> a, "sig" -> sig, "SP" -> sp)
+    } yield (Static(mu, a, sig), List(sp))
 
-    def addTimePoint(current: RandomVariable[Map[String, Real]], i: Int) =
+    def addTimePoint(current: RandomVariable[(Static, List[Real])], i: Int) =
       for {
-        map <- current
-        os = if (i == 0) map("SP") else map(f"S$i%03d")
+        tup <- current
+        static = tup._1
+        states = tup._2
+        os = states.head
         ni = i + 1
-        ns <- Normal(map("mu") + map("a") * (os - map("mu")), map("sig")).param
+        ns <- Normal(static.mu + static.a * (os - static.mu), static.sig).param
         _ <- Poisson(ns.exp).fit(obs(i))
-      } yield map.updated(f"S$ni%03d", ns)
+      } yield (static, ns :: states)
 
     val fullModel = (0 until n).foldLeft(prior)(addTimePoint(_, _))
 
     val model = for {
-      map <- fullModel
+      tup <- fullModel
+      static = tup._1
+      states = tup._2
     } yield
       Map(
-        "mu" -> map("mu"),
-        "a" -> map("a"),
-        "sig" -> map("sig"),
-        "SP" -> map("SP")
+        "mu" -> static.mu,
+        "a" -> static.a,
+        "sig" -> static.sig,
+        "SP" -> states.reverse.head
       )
 
     println("Model built. Sampling now...")
-    //val out = model.sample()
-    //val out = model.sample(Walkers(100), 1000, 2000)
-    val out = model.sample(HMC(10), 100, 200)
-    //println("Sampling finished.")
+    val out = model.sample(HMC(5), 1000, 10000, 1)
 
     println("Iterates: " + out.length)
     println("First 20:")
@@ -84,7 +87,6 @@ object DGLM {
           (r("mu"), r("sig"))
         })
         .mkString("\n"))
-    //Report.printReport(model, Emcee(5000, 2000, 200))
 
   }
 

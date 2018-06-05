780ad78ee107fdeaaad8a65c542c071bdd8983c6
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-06-05 23:47:22 +0100

    dlm for blog post

---
+++ scala/rainier-test/src/main/scala/DLM3.scala
@@ -0,0 +1,87 @@
+import com.stripe.rainier.compute._
+import com.stripe.rainier.core._
+import com.stripe.rainier.sampler._
+
+object DLM3 {
+
+  def main(args: Array[String]): Unit = {
+
+    // first simulate some data from a DLM model
+    implicit val rng = ScalaRNG(1)
+    val n = 75 // number of observations/time points
+    val mu = 3.0 // AR(1) mean
+    val a = 0.95 // auto-regressive parameter
+    val sig = 1.0 // AR(1) SD
+    val sigD = 3.0 // observational SD
+    val state = Stream
+      .iterate(0.0)(x => mu + (x - mu) * a + sig * rng.standardNormal)
+      .take(n)
+      .toVector
+    val obs = state.map(_ + sigD * rng.standardNormal)
+
+    // build and fit model
+    case class Static(mu: Real, a: Real, sig: Real, sigD: Real)
+
+    val prior = for {
+      mu <- Normal(0, 10).param
+      a <- Normal(1, 0.1).param
+      sig <- Gamma(2,1).param
+      sigD <- Gamma(2,2).param
+      sp <- Normal(0, 50).param
+    } yield (Static(mu, a, sig, sigD), List(sp))
+
+    def addTimePoint(current: RandomVariable[(Static, List[Real])],
+                     datum: Double): RandomVariable[(Static, List[Real])] =
+      for {
+        tup <- current
+        static = tup._1
+        states = tup._2
+        os = states.head
+        ns <- Normal(((Real.one - static.a) * static.mu) + (static.a * os),
+                     static.sig).param
+        _ <- Normal(ns, static.sigD).fit(datum)
+      } yield (static, ns :: states)
+
+    val fullModel = obs.foldLeft(prior)(addTimePoint(_, _))
+
+    val model = for {
+      tup <- fullModel
+      static = tup._1
+      states = tup._2
+    } yield
+      Map("mu" -> static.mu,
+          "a" -> static.a,
+          "sig" -> static.sig,
+          "sigD" -> static.sigD,
+          "SP" -> states.reverse.head)
+
+    // sampling
+    println("Model built. Sampling now (will take a long time)...")
+    val thin = 500
+    val out = model.sample(HMC(4), 100000, 10000 * thin, thin)
+    println("Sampling finished.")
+
+    // some diagnostic plots
+    import com.cibo.evilplot.geometry.Extent
+    import com.stripe.rainier.plot.EvilTracePlot._
+
+    render(traces(out,
+                  truth = Map("mu" -> mu,
+                              "a" -> a,
+                              "sigD" -> sigD,
+                              "sig" -> sig,
+                              "SP" -> state(0))),
+           "traceplots.png",
+           Extent(1200, 1400))
+    render(pairs(out,
+                 truth = Map("mu" -> mu,
+                             "a" -> a,
+                             "sigD" -> sigD,
+                             "sig" -> sig,
+                             "SP" -> state(0))),
+           "pairs.png")
+    println("Diagnostic plots written to disk")
+
+  }
+
+}

e36998b3bafe419bc9c56bf31e0ef509165b84f5
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-07-10 23:08:36 +0100

    starting on some abc functions

--- r/abc/abc.R
+++ r/abc/abc.R
@@ -2,9 +2,28 @@
 
 library(smfsb)
 
+## Generic code
 
 
 
+
+
+
+
+## Example code
+
+data(LVdata)
+
+rprior = function() { exp(runif(3, -4, 2)) }
+
+rmodel = function(th) { simTs(rpois(2,50), 0, 30, 2, stepLVc, th) }
+## compare to LVperfect...
+sumStats = identity
+ssd = sumStats(LVperfect)
+distance = function(s) {
+    diff = s - ssd
+    sqrt(sum(diff*diff))
+}
 
 
 ## eof

bcb1f9462bc892f26c00becba8630b94d1c700ac
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2018-07-13 09:32:50 +0100

    separate out the parallel version, which doesn't seem to be giving correct results - PPRNG issue?

--- r/pmmh/pPMCMC.R
+++ r/pmmh/pPMCMC.R
@@ -3,6 +3,34 @@
 ## make sure the package is loaded
 require(smfsb)
 
+require(parallel)
+
+## code to factor out
+ppfMLLik = function (n, simx0, t0, stepFun, dataLik, data) 
+{
+    times = c(t0, as.numeric(rownames(data)))
+    deltas = diff(times)
+    return(function(...) {
+        xmat = simx0(n, t0, ...)
+        ll = 0
+        for (i in 1:length(deltas)) {
+            xmat = t(parApply(cl, xmat, 1, stepFun, t0 = times[i],
+                             deltat = deltas[i], ...))
+            lw = parRapply(cl, xmat, dataLik, t = times[i + 1],
+                           y = data[i,], log = TRUE, ...)
+            m = max(lw)
+            rw = lw - m
+            sw = exp(rw)
+            ll = ll + m + log(mean(sw))
+            rows = sample(1:n, n, replace = TRUE, prob = sw)
+            xmat = xmat[rows,]
+        }
+        ll
+    })
+}
+## end of code to factor out
+
+
 ## load the reference data
 data(LVdata)
 
@@ -48,9 +76,9 @@ LVpreyData=as.timedData(LVpreyNoise10)
 colnames(LVpreyData)=c("x1")
 
 ## create marginal log-likelihood functions, based on a particle filter
-mLLik1=pfMLLik(100,simx0,0,stepLVc,data1Lik,LVpreyData)
-mLLik2=pfMLLik(100,simx0,0,stepLVc,data2Lik,LVdata)
-mLLik3=pfMLLik(100,simx0,0,stepLVc,data3Lik,LVdata)
+mLLik1=ppfMLLik(100,simx0,0,stepLVc,data1Lik,LVpreyData)
+mLLik2=ppfMLLik(100,simx0,0,stepLVc,data2Lik,LVdata)
+mLLik3=ppfMLLik(100,simx0,0,stepLVc,data3Lik,LVdata)
 
 ## Now create an MCMC algorithm...
 th=c(th1 = 1, th2 = 0.005, th3 = 0.6)
@@ -60,7 +88,7 @@ rprop = function(th, tune=0.01) { th*exp(rnorm(p,0,tune)) }
 
 cl = makeCluster(4)
 clusterExport(cl,c("noiseSD"))
-thmat = metropolisHastings(th,mLLik1,rprop,iters=1000,thin=10)
+thmat = metropolisHastings(th,mLLik1,rprop,iters=10000,thin=10)
 stopCluster(cl)
 
 ## Dump MCMC output matrix to disk

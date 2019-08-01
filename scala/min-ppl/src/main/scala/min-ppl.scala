a1dec6dd34828b8883ffec20528d2b7fb47b0fe5
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2019-08-01 15:37:31 +0100

    language fixed and working! Need some good examples.

--- scala/min-ppl/src/main/scala/min-ppl.scala
+++ scala/min-ppl/src/main/scala/min-ppl.scala
@@ -26,7 +26,7 @@ object MinPpl {
   }
 
   trait Prob[T] {
-    val particles: Array[Particle[T]]
+    val particles: Vector[Particle[T]]
     def map[S](f: T => S): Prob[S] = Empirical(particles map (_ map f))
     def flatMap[S](f: T => Prob[S]): Prob[S] = {
       Empirical((particles map (p => {
@@ -41,38 +41,38 @@ object MinPpl {
       val law = mx + math.log(rw.sum/(rw.length))
       val ind = bdist.Multinomial(DenseVector(rw.toArray)).sample(N)
       val newParticles = ind map (i => particles(i))
-      Empirical(newParticles.toArray map (pi => Particle(pi.v, law)))
+      Empirical(newParticles.toVector map (pi => Particle(pi.v, law)))
     }
     def cond(ll: T => Double): Prob[T] =
       Empirical(particles map (p => Particle(p.v, p.lw + ll(p.v))))
-    def empirical: Array[T] = resample.particles.map(_.v)
+    def empirical: Vector[T] = resample.particles.map(_.v)
   }
 
-  case class Empirical[T](particles: Array[Particle[T]]) extends Prob[T]
+  case class Empirical[T](particles: Vector[Particle[T]]) extends Prob[T]
 
-  def unweighted[T](ts: Array[T]): Prob[T] = Empirical(ts map (Particle(_, 0.0)))
+  def unweighted[T](ts: Vector[T]): Prob[T] = Empirical(ts map (Particle(_, 0.0)))
 
   trait Dist[T] extends Prob[T] {
     def ll(obs: T): Double
     def ll(obs: Seq[T]): Double = obs map (ll) reduce (_+_)
     //def fit(obs: Seq[T]): Prob[T] = Empirical(particles map (p => Particle(p.v, p.lw + ll(obs))))
-    def fit(obs: Seq[T]): Prob[T] = Empirical(Array(Particle(obs.head, ll(obs))))
+    def fit(obs: Seq[T]): Prob[T] = Empirical(Vector(Particle(obs.head, ll(obs))))
     def fit(obs: T): Prob[T] = fit(List(obs))
-    //def fit(obs: T): Prob[T] = Empirical(Array(Particle(obs, ll(obs))))
+    //def fit(obs: T): Prob[T] = Empirical(Vector(Particle(obs, ll(obs))))
   }
 
   case class Normal(mu: Double, v: Double)(implicit N: Int) extends Dist[Double] {
-    lazy val particles = unweighted(bdist.Gaussian(mu, math.sqrt(v)).sample(N).toArray).particles
+    lazy val particles = unweighted(bdist.Gaussian(mu, math.sqrt(v)).sample(N).toVector).particles
     def ll(obs: Double) = bdist.Gaussian(mu, math.sqrt(v)).logPdf(obs)
   }
 
   case class Gamma(a: Double, b: Double)(implicit N: Int) extends Prob[Double] {
-    lazy val particles = unweighted(bdist.Gamma(a, 1.0/b).sample(N).toArray).particles
+    lazy val particles = unweighted(bdist.Gamma(a, 1.0/b).sample(N).toVector).particles
     def ll(obs: Double) = bdist.Gamma(a, 1.0/b).logPdf(obs)
   }
 
   case class Poisson(mu: Double)(implicit N: Int) extends Prob[Int] {
-    lazy val particles = unweighted(bdist.Poisson(mu).sample(N).toArray).particles
+    lazy val particles = unweighted(bdist.Poisson(mu).sample(N).toVector).particles
     def ll(obs: Int) = bdist.Poisson(mu).logProbabilityOf(obs)
   }
 

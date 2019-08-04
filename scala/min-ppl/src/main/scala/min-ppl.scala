2baa29f50db3b6067df76c1cadabe38a0fa6a813
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2019-08-04 18:31:45 +0100

    linear model example working

--- scala/min-ppl/src/main/scala/min-ppl.scala
+++ scala/min-ppl/src/main/scala/min-ppl.scala
@@ -16,7 +16,8 @@ object MinPpl {
   //implicit val numParticles = 100
   implicit val numParticles = 300
   //implicit val numParticles = 1000
-
+  //implicit val numParticles = 2000
+  
   case class Particle[T](v: T, lw: Double) { // value and log-weight
     def map[S](f: T => S): Particle[S] = Particle(f(v), lw)
     // TODO: Don't need flatMap in min version
@@ -87,6 +88,8 @@ object MinPpl {
 
   // Now a few simple examples
 
+  import breeze.stats.{meanAndVariance => meanVar}
+
   // Linear Gaussian
   def example1 = {
     val xy = for {
@@ -96,14 +99,14 @@ object MinPpl {
     val y = xy.map(_._2)
     val yGz = y.cond(yi => Normal(yi, 9).ll(8.0)).empirical
     print("y: 5.000, 5.000 : ")
-    println(breeze.stats.meanAndVariance(y.empirical))
+    println(meanVar(y.empirical))
     print("y: 6.071, 3.214 : ")
-    println(breeze.stats.meanAndVariance(yGz))
+    println(meanVar(yGz))
     val xyGz = xy.cond{case (x,y) => Normal(y,9).ll(8.0)}.empirical
     print("x: 5.857, 2.867 : ")
-    println(breeze.stats.meanAndVariance(xyGz.map(_._1))) // x
+    println(meanVar(xyGz.map(_._1))) // x
     print("y: 6.071, 3.214 : ")
-    println(breeze.stats.meanAndVariance(xyGz.map(_._2))) // y
+    println(meanVar(xyGz.map(_._2))) // y
     // Now cond inside for expression...
     val xyz = for {
       x <- Normal(5,4)
@@ -111,9 +114,9 @@ object MinPpl {
     } yield (x,y)
     val xyze = xyz.empirical
     print("x: 5.857, 2.867 : ")
-    println(breeze.stats.meanAndVariance(xyze.map(_._1))) // x
+    println(meanVar(xyze.map(_._1))) // x
     print("y: 6.071, 3.214 : ")
-    println(breeze.stats.meanAndVariance(xyze.map(_._2))) // y
+    println(meanVar(xyze.map(_._2))) // y
     // Now cond inside a deeper for expression...
     val wxyz = for {
       w <- Normal(5,2)
@@ -122,11 +125,11 @@ object MinPpl {
     } yield (w,x,y)
     val wxyze = wxyz.empirical
     print("w: 5.429, 1.714 : ")
-    println(breeze.stats.meanAndVariance(wxyze.map(_._1))) // w
+    println(meanVar(wxyze.map(_._1))) // w
     print("x: 5.857, 2.867 : ")
-    println(breeze.stats.meanAndVariance(wxyze.map(_._2))) // x
+    println(meanVar(wxyze.map(_._2))) // x
     print("y: 6.071, 3.214 : ")
-    println(breeze.stats.meanAndVariance(wxyze.map(_._3))) // y
+    println(meanVar(wxyze.map(_._3))) // y
     // Now fit...
     val xyzf = for {
       x <- Normal(5,4)
@@ -135,11 +138,11 @@ object MinPpl {
     } yield (x,y,z)
     val xyzfe = xyzf.empirical
     print("x: 5.857, 2.867 : ")
-    println(breeze.stats.meanAndVariance(xyzfe.map(_._1))) // x
+    println(meanVar(xyzfe.map(_._1))) // x
     print("y: 6.071, 3.214 : ")
-    println(breeze.stats.meanAndVariance(xyzfe.map(_._2))) // y
+    println(meanVar(xyzfe.map(_._2))) // y
     print("z: 6.071,12.214 : ")
-    println(breeze.stats.meanAndVariance(xyzfe.map(_._3))) // z
+    println(meanVar(xyzfe.map(_._3))) // z
     // Now fitQ...
     val xyzfq = for {
       x <- Normal(5,4)
@@ -148,11 +151,11 @@ object MinPpl {
     } yield (x,y,z)
     val xyzfqe = xyzfq.empirical
     print("x: 5.857, 2.867 : ")
-    println(breeze.stats.meanAndVariance(xyzfqe.map(_._1))) // x
+    println(meanVar(xyzfqe.map(_._1))) // x
     print("y: 6.071, 3.214 : ")
-    println(breeze.stats.meanAndVariance(xyzfqe.map(_._2))) // y
+    println(meanVar(xyzfqe.map(_._2))) // y
     print("z: 8.000, 0.000 : ")
-    println(breeze.stats.meanAndVariance(xyzfqe.map(_._3))) // z
+    println(meanVar(xyzfqe.map(_._3))) // z
     // Simpler fit test
     val yzf = for {
       y <- Normal(5,5)
@@ -160,9 +163,9 @@ object MinPpl {
     } yield (y,z)
     val yzfe = yzf.empirical
     print("y: 6.071, 3.214 : ")
-    println(breeze.stats.meanAndVariance(yzfe.map(_._1))) // y
+    println(meanVar(yzfe.map(_._1))) // y
     print("z: 6.071,12.214 : ")
-    println(breeze.stats.meanAndVariance(yzfe.map(_._2))) // z
+    println(meanVar(yzfe.map(_._2))) // z
     // Simpler fit test - multiple obs
     val yzf2 = for {
       y <- Normal(5,5)
@@ -170,9 +173,9 @@ object MinPpl {
     } yield (y,z)
     val yzfe2 = yzf2.empirical
     print("y: 6.071, 3.214 : ")
-    println(breeze.stats.meanAndVariance(yzfe2.map(_._1))) // y
+    println(meanVar(yzfe2.map(_._1))) // y
     print("z: 6.071,21.214 : ")
-    println(breeze.stats.meanAndVariance(yzfe2.map(_._2))) // z
+    println(meanVar(yzfe2.map(_._2))) // z
   }
 
   def example1a = {
@@ -183,7 +186,7 @@ object MinPpl {
       z <- Normal(y,1)
     } yield z
     println("2.0, 5.0 :")
-    println(breeze.stats.meanAndVariance(deep.empirical))
+    println(meanVar(deep.empirical))
   }
 
   // Normal random sample
@@ -195,9 +198,9 @@ object MinPpl {
     } yield (mu,v)
     val modEmp = mod.empirical
     print("mu : ")
-    println(breeze.stats.meanAndVariance(modEmp map (_._1)))
+    println(meanVar(modEmp map (_._1)))
     print("v : ")
-    println(breeze.stats.meanAndVariance(modEmp map (_._2)))
+    println(meanVar(modEmp map (_._2)))
   }
 
   // Normal random sample - IG on v
@@ -209,12 +212,12 @@ object MinPpl {
     } yield (mu,tau)
     val modEmp = mod.empirical
     print("mu : ")
-    println(breeze.stats.meanAndVariance(modEmp map (_._1)))
+    println(meanVar(modEmp map (_._1)))
     print("tau : ")
-    println(breeze.stats.meanAndVariance(modEmp map (_._2)))
+    println(meanVar(modEmp map (_._2)))
   }
 
-  // TODO: Poisson DGLM
+  // Poisson DGLM
   def example3 = {
 
     val data = List(2,1,0,2,3,4,5,4,3,2,1)
@@ -232,29 +235,47 @@ object MinPpl {
         (w, states) = tup
         os = states.head
         ns <- Normal(os, w)
-        _ <- Poisson(math.exp(ns)).fit(obs)
+        _ <- Poisson(math.exp(ns)).fitQ(obs)
       } yield (w, ns :: states)
     }
 
     val mod = data.foldLeft(prior)(addTimePoint(_,_)).empirical
     print("w  : ")
-    println(breeze.stats.meanAndVariance(mod map (_._1)))
+    println(meanVar(mod map (_._1)))
     print("s0 : ")
-    println(breeze.stats.meanAndVariance(mod map (_._2.reverse.head)))
+    println(meanVar(mod map (_._2.reverse.head)))
     print("sN : ")
-    println(breeze.stats.meanAndVariance(mod map (_._2.head)))
+    println(meanVar(mod map (_._2.head)))
 
   }
 
-
-
-
-  // TODO: Linear model
+  // Linear model
   def example4 = {
-
     val x = List(1.0,2,3,4,5,6)
     val y = List(3.0,2,4,5,5,6)
-
+    val xy = x zip y
+    case class Param(alpha: Double, beta: Double, v: Double)
+    println("Forming prior distribution")
+    val prior = for {
+      alpha <- Normal(0,10)
+      beta <- Normal(0,4)
+      v <- Gamma(1,0.1)
+    } yield Param(alpha, beta, v)
+    def addPoint(current: Prob[Param], obs: (Double, Double)): Prob[Param] = {
+      println(s"Conditioning on $obs")
+      for {
+        p <- current
+        (x, y) = obs
+        _ <- Normal(p.alpha + p.beta * x, p.v).fitQ(y)
+      } yield p
+    }
+    val mod = xy.foldLeft(prior)(addPoint(_,_)).empirical
+    print("a : ")
+    println(meanVar(mod map (_.alpha)))
+    print("b : ")
+    println(meanVar(mod map (_.beta)))
+    print("v : ")
+    println(meanVar(mod map (_.v)))
   }
 
 
@@ -264,7 +285,7 @@ object MinPpl {
 
   def main(args: Array[String]): Unit = {
     println("Hi")
-    example3
+    example4
     println("Bye")
   }
 

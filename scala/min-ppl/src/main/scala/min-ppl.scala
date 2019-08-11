3f1f653b457017b30db82ae9010671720d730a9f
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2019-08-11 11:02:10 +0100

    update to min-ppl2 version of code

--- scala/min-ppl/src/main/scala/min-ppl.scala
+++ scala/min-ppl/src/main/scala/min-ppl.scala
@@ -1,53 +1,68 @@
 /*
 min-ppl.scala
-A minimal probabilistic programming language
-*/
 
-object MinPpl {
+SMC-based probability monad for bootstrap particle filter inference
+
+ */
+
+object MinPpl2 {
 
   import breeze.stats.{distributions => bdist}
   import breeze.linalg.DenseVector
+  import cats._
+  import cats.implicits._
 
-  //implicit val numParticles = 100
-  implicit val numParticles = 300
-  //implicit val numParticles = 1000
-  //implicit val numParticles = 2000
-  
+  implicit val numParticles = 2000
+
   case class Particle[T](v: T, lw: Double) { // value and log-weight
     def map[S](f: T => S): Particle[S] = Particle(f(v), lw)
-    // TODO: Don't need flatMap in min version
-    // TODO: OR, re-write Prob.flatMap to use it...
     def flatMap[S](f: T => Particle[S]): Particle[S] = {
       val ps = f(v)
       Particle(ps.v, lw + ps.lw)
     }
   }
 
+  implicit val particleMonad = new Monad[Particle] {
+    def pure[T](t: T): Particle[T] = Particle(t, 0.0)
+    def flatMap[T,S](pt: Particle[T])(f: T => Particle[S]): Particle[S] = pt.flatMap(f)
+    def tailRecM[T,S](t: T)(f: T => Particle[Either[T,S]]): Particle[S] = ???
+  }
+
   trait Prob[T] {
     val particles: Vector[Particle[T]]
-    def map[S](f: T => S): Prob[S] = Empirical(particles map (_ map f))
-    def flatMap[S](f: T => Prob[S]): Prob[S] = {
-      Empirical((particles map (p => {
-        f(p.v).particles.map(psi => Particle(psi.v, p.lw + psi.lw))
-      })).flatten).resample
-    }
+    def draw: Particle[T]
+    def mapP[S](f: T => Particle[S]): Prob[S] = Empirical(particles map (_ flatMap f))
+    def map[S](f: T => S): Prob[S] = mapP(v => Particle(f(v), 0.0))
+    def flatMap[S](f: T => Prob[S]): Prob[S] = mapP(f(_).draw)
     def resample(implicit N: Int): Prob[T] = {
       val lw = particles map (_.lw)
       val mx = lw reduce (math.max(_,_))
-      //val np = particles.length ; println(s"$np $mx") // TODO: Debug code
       val rw = lw map (lwi => math.exp(lwi - mx))
       val law = mx + math.log(rw.sum/(rw.length))
       val ind = bdist.Multinomial(DenseVector(rw.toArray)).sample(N)
       val newParticles = ind map (i => particles(i))
       Empirical(newParticles.toVector map (pi => Particle(pi.v, law)))
     }
-    // TODO: don't strictly need "cond" in a minimal PPL...
-    def cond(ll: T => Double): Prob[T] =
-      Empirical(particles map (p => Particle(p.v, p.lw + ll(p.v))))
+    def cond(ll: T => Double): Prob[T] = mapP(v => Particle(v, ll(v)))
     def empirical: Vector[T] = resample.particles.map(_.v)
   }
 
-  case class Empirical[T](particles: Vector[Particle[T]]) extends Prob[T]
+  implicit val probMonad = new Monad[Prob] {
+    def pure[T](t: T): Prob[T] = Empirical(Vector(Particle(t, 0.0)))
+    def flatMap[T,S](pt: Prob[T])(f: T => Prob[S]): Prob[S] = pt.flatMap(f)
+    def tailRecM[T,S](t: T)(f: T => Prob[Either[T,S]]): Prob[S] = ???
+  }
+
+  case class Empirical[T](particles: Vector[Particle[T]]) extends Prob[T] {
+    def draw: Particle[T] = {
+      val lw = particles map (_.lw)
+      val mx = lw reduce (math.max(_,_))
+      val rw = lw map (lwi => math.exp(lwi - mx))
+      val law = mx + math.log(rw.sum/(rw.length))
+      val idx = bdist.Multinomial(DenseVector(rw.toArray)).draw
+      Particle(particles(idx).v, law)
+    }
+  }
 
   def unweighted[T](ts: Vector[T], lw: Double = 0.0): Prob[T] =
     Empirical(ts map (Particle(_, lw)))
@@ -55,29 +70,35 @@ object MinPpl {
   trait Dist[T] extends Prob[T] {
     def ll(obs: T): Double
     def ll(obs: Seq[T]): Double = obs map (ll) reduce (_+_)
-    def fit(obs: Seq[T]): Prob[T] =
-      Empirical(particles map (p => Particle(p.v, p.lw + ll(obs))))
+    def fit(obs: Seq[T]): Prob[T] = mapP(v => Particle(v, ll(obs)))
     def fitQ(obs: Seq[T]): Prob[T] = Empirical(Vector(Particle(obs.head, ll(obs))))
     def fit(obs: T): Prob[T] = fit(List(obs))
     def fitQ(obs: T): Prob[T] = fitQ(List(obs))
   }
 
   case class Normal(mu: Double, v: Double)(implicit N: Int) extends Dist[Double] {
-    lazy val particles = unweighted(bdist.Gaussian(mu, math.sqrt(v)).sample(N).toVector).particles
+    lazy val particles = unweighted(bdist.Gaussian(mu, math.sqrt(v)).
+      sample(N).toVector).particles
+    def draw = Particle(bdist.Gaussian(mu, math.sqrt(v)).draw, 0.0)
     def ll(obs: Double) = bdist.Gaussian(mu, math.sqrt(v)).logPdf(obs)
   }
 
   case class Gamma(a: Double, b: Double)(implicit N: Int) extends Dist[Double] {
-    lazy val particles = unweighted(bdist.Gamma(a, 1.0/b).sample(N).toVector).particles
+    lazy val particles = unweighted(bdist.Gamma(a, 1.0/b).
+      sample(N).toVector).particles
+    def draw = Particle(bdist.Gamma(a, 1.0/b).draw, 0.0)
     def ll(obs: Double) = bdist.Gamma(a, 1.0/b).logPdf(obs)
   }
 
   case class Poisson(mu: Double)(implicit N: Int) extends Dist[Int] {
-    lazy val particles = unweighted(bdist.Poisson(mu).sample(N).toVector).particles
+    lazy val particles = unweighted(bdist.Poisson(mu).
+      sample(N).toVector).particles
+    def draw = Particle(bdist.Poisson(mu).draw, 0.0)
     def ll(obs: Int) = bdist.Poisson(mu).logProbabilityOf(obs)
   }
 
 }
+
 
 // eof
 
--- scala/min-ppl/src/test/scala/min-ppl-test.scala
+++ scala/min-ppl/src/test/scala/min-ppl-test.scala
@@ -1,12 +1,128 @@
-import org.scalatest.FlatSpec
+/*
+min-ppl-test.scala
 
-class SetSpec extends FlatSpec {
+Some basic sanity checks on the language
 
- "A Poisson(10.0)" should "have mean 10.0" in {
-    import breeze.stats.distributions.Poisson
-    val p = Poisson(10.0)
-    val m = p.mean
-    assert(math.abs(m - 10.0) < 0.000001)
+ */
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalactic._
+import MinPpl2._
+import breeze.stats.{meanAndVariance => meanVar}
+
+class PplSpec extends AnyFlatSpec with Tolerance {
+
+  "A linear Gaussian" should "flatMap correctly" in {
+    System.err.println("**These tests take a LONG time, and it is normal for a couple to fail**")
+    val xy = for {
+      x <- Normal(5,4)
+      y <- Normal(x,1)
+    } yield (x,y)
+    val y = xy.map(_._2).empirical
+    val mv = meanVar(y)
+    assert(mv.mean === 5.0 +- 0.2)
+    assert(mv.variance === 5.0 +- 0.5)
+  }
+
+  it should "cond correctly" in {
+    val xy = for {
+      x <- Normal(5,4)
+      y <- Normal(x,1)
+    } yield (x,y)
+    val y = xy.map(_._2)
+    val yGz = y.cond(yi => Normal(yi, 9).ll(8.0)).empirical
+    val mv = meanVar(yGz)
+    assert(mv.mean === 5.857 +- 0.2)
+    assert(mv.variance === 2.867 +- 0.5)
+    val xyGz = xy.cond{case (x,y) => Normal(y,9).ll(8.0)}.empirical
+    val mvx = meanVar(xyGz.map(_._1))
+    assert(mvx.mean === 5.857 +- 0.2)
+    assert(mvx.variance === 2.867 +- 0.5)
+    val mvy = meanVar(xyGz.map(_._2))
+    assert(mvy.mean === 6.071 +- 0.2)
+    assert(mvy.variance === 3.214 +- 0.5)
+  }
+
+  it should "cond correctly in a for" in {
+    val wxyz = for {
+      w <- Normal(5,2)
+      x <- Normal(w,2)
+      y <- Normal(x,1).cond(y => Normal(y,9).ll(8.0))
+    } yield (w,x,y)
+    val wxyze = wxyz.empirical
+    val mvw = meanVar(wxyze.map(_._1))
+    assert(mvw.mean === 5.429 +- 0.2)
+    assert(mvw.variance === 1.714 +- 0.5)
+    val mvx = meanVar(wxyze.map(_._2))
+    assert(mvx.mean === 5.857 +- 0.2)
+    assert(mvx.variance === 2.867 +- 0.5)
+    val mvy = meanVar(wxyze.map(_._3))
+    assert(mvy.mean === 6.071 +- 0.2)
+    assert(mvy.variance === 3.214 +- 0.5)
+  }
+
+  it should "fit correctly" in {
+    val xyzf = for {
+      x <- Normal(5,4)
+      y <- Normal(x,1)
+      z <- Normal(y,9).fit(8.0)
+    } yield (x,y,z)
+    val xyzfe = xyzf.empirical
+    val mvx = meanVar(xyzfe.map(_._1))
+    assert(mvx.mean === 5.857 +- 0.2)
+    assert(mvx.variance === 2.867 +- 0.5)
+    val mvy = meanVar(xyzfe.map(_._2))
+    assert(mvy.mean === 6.071 +- 0.2)
+    assert(mvy.variance === 3.214 +- 0.5)
+    val mvz = meanVar(xyzfe.map(_._3))
+    assert(mvz.mean === 6.071 +- 0.2)
+    assert(mvz.variance === 12.214 +- 1.0)
+  }
+
+  it should "fitQ correctly" in {
+    val xyzfq = for {
+      x <- Normal(5,4)
+      y <- Normal(x,1)
+      z <- Normal(y,9).fitQ(8.0)
+    } yield (x,y,z)
+    val xyzfqe = xyzfq.empirical
+    val mvx = meanVar(xyzfqe.map(_._1))
+    assert(mvx.mean === 5.857 +- 0.2)
+    assert(mvx.variance === 2.867 +- 0.5)
+    val mvy = meanVar(xyzfqe.map(_._2))
+    assert(mvy.mean === 6.071 +- 0.2)
+    assert(mvy.variance === 3.214 +- 0.5)
+    val mvz = meanVar(xyzfqe.map(_._3))
+    assert(mvz.mean === 8.000 +- 0.001)
+    assert(mvz.variance === 0.000 +- 0.001)
+  }
+
+  it should "fit marginalised correctly" in {
+    val yzf = for {
+      y <- Normal(5,5)
+      z <- Normal(y,9).fit(8.0)
+    } yield (y,z)
+    val yzfe = yzf.empirical
+    val mvy = meanVar(yzfe.map(_._1))
+    assert(mvy.mean === 6.071 +- 0.2)
+    assert(mvy.variance === 3.213 +- 0.5)
+    val mvz = meanVar(yzfe.map(_._2))
+    assert(mvz.mean === 6.071 +- 0.2)
+    assert(mvz.variance === 12.214 +- 1.0)
+  }
+
+  it should "fit multiple iid observations correctly" in {
+    val yzf2 = for {
+      y <- Normal(5,5)
+      z <- Normal(y,18).fit(List(6.0,10.0))
+    } yield (y,z)
+    val yzfe2 = yzf2.empirical
+    val mvy = meanVar(yzfe2.map(_._1))
+    assert(mvy.mean === 6.071 +- 0.2)
+    assert(mvy.variance === 3.214 +- 0.5)
+    val mvz = meanVar(yzfe2.map(_._2))
+    assert(mvz.mean === 6.071 +- 0.2)
+    assert(mvz.variance === 21.214 +- 1.5)
   }
 
 }

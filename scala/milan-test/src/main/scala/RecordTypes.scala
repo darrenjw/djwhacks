7244791228ab99b9f0592ace7046ae0319d98d01
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2020-01-07 18:17:39 +0000

    failing to get example to build - loggging library conflict of some sort

---
+++ scala/milan-test/src/main/scala/RecordTypes.scala
@@ -0,0 +1,34 @@
+
+import java.time.Instant
+
+import com.amazon.milan.Id
+
+
+class DateValueRecord(var recordId: String,
+                      var dateTime: Instant,
+                      var value: Double) {
+  def this(dateTime: Instant, value: Double) {
+    this(Id.newId(), dateTime, value)
+  }
+
+  override def toString: String = s"(${this.dateTime}, ${this.value}))"
+}
+
+object DateValueRecord {
+  def apply(dateTime: Instant, value: Double): DateValueRecord = new DateValueRecord(dateTime, value)
+}
+
+
+class KeyValueRecord(var recordId: String,
+                     var key: Int,
+                     var value: Int) {
+  def this(key: Int, value: Int) {
+    this(Id.newId(), key, value)
+  }
+
+  override def toString: String = s"(${this.key}, ${this.value}))"
+}
+
+object KeyValueRecord {
+  def apply(key: Int, value: Int): KeyValueRecord = new KeyValueRecord(key, value)
+}

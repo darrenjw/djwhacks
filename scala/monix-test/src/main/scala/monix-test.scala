7cc71d89f46058f2f13c3e07a914591c2ebddeaf
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2020-01-07 22:27:46 +0000

    trivial example

--- scala/monix-test/src/main/scala/monix-test.scala
+++ scala/monix-test/src/main/scala/monix-test.scala
@@ -8,9 +8,14 @@ object CatsApp {
   import cats._
   import cats.implicits._
 
+  import monix.reactive._
+  import monix.execution.Scheduler.Implicits.global
+  import scala.concurrent.duration._
+
   def main(args: Array[String]): Unit = {
-    val l = List(1,2) |+| List(3,4)
-    println(l)
+    Observable(1,2,3,4,5).
+      consumeWith(Consumer.foreach(a => println(a))).
+      runSyncUnsafe(5.seconds)
   }
 
 }

bd965312810b794bb18a7b9ee77ae2973851eb0a
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2021-12-10 20:27:55 +0000

    tweak

--- python/tk-canvas.py
+++ python/tk-canvas.py
@@ -5,5 +5,10 @@ root = Tk()
 c = Canvas(root, width=1000, height=1000)
 c.pack()
 
-
 c.create_rectangle(100, 100, 200, 200, fill="blue", outline = 'black')
+c.create_line(100, 300, 300, 300, fill="#990000", width=3)
+c.create_oval(100, 400, 150, 450, fill="red", outline = "black")
+c.create_oval(200, 400, 201, 401, fill="black", outline="black")
+c.create_polygon([300, 400, 400, 400, 350, 500], fill="red", outline="black")
+
+mainloop()

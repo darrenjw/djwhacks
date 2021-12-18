481a9f4f34f3f291949294e3fc3dc8bb79e16b13
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2021-12-18 16:05:20 +0000

    working diamond-square algorithm

--- python/fractals/fbm2.py
+++ python/fractals/fbm2.py
@@ -6,18 +6,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Choose N and H:
-N = 6
-H = 0.9
+N = 10
+H = 0.85
 
 
 # Derived quantities
-n = 2**N
+n = int(2**N) + 1
 D = 3 - H
 
 # Print some summary info
 print(f'N={N}, n={n}, H={H}, D={D}')
-if (N > 6):
-    print("***Large N*** - will take a while. Be patient!")
 
 # Create an empty matrix to fill with the fractal    
 mat = np.zeros([n, n])
@@ -31,49 +29,36 @@ def m(r,c):
         r = r - n
     if (c >= n):
         c = c - n
-    r = int(r)
-    c = int(c)
+    r = r
+    c = c
     return(mat[r,c])
 
-# TODO: recursion not right - overlapping subdomains!
+step = n-1
+for L in range(N):
+    print(f'Level {L}')
+    step = step//2
+    # diamond step
+    for i in range(int(2**L)):
+        for j in range(int(2**L)):
+            r = (2*i+1)*step
+            c = (2*j+1)*step
+            mean = 0.25*(m(r-step,c-step)+m(r-step,c+step)+m(r+step,c-step)+m(r+step,c+step))
+            mat[r,c] = mean + np.random.normal()*(2**(-H*L))
+    # square step
+    for i in range(int(2**(L+1))+1):
+        for j in range(int(2**(L+1))+1):
+            if ((i+j)%2 == 1):
+                r = i*step
+                c = j*step
+                mean = 0.25*(m(r,c-step)+m(r,c+step)+m(r-step,c)+m(r+step,c))
+                mat[r,c] = mean + np.random.normal()*(2**(-H*L))
 
-def squareFill(L, tlr, tlc, brr, brc):
-    #print(f'squareFill({L},{tlr},{tlc},{brr},{brc})')
-    shift = (brr - tlr)/2
-    cr = tlr + shift
-    cc = tlc + shift
-    if ((cr < 0)|(cr >= n)|(cc < 0)|(cc >= n)):
-        return
-    mean = 0.25*(m(tlr,tlc)+m(tlr,brc)+m(brr,tlc)+m(brr,brc))
-    mat[int(cr),int(cc)] = mean + np.random.normal()*2**(-H*(N-L))
-    if (L > 0):
-        diamondFill(L, tlr, tlc-shift, brr, brc-shift)
-        diamondFill(L, tlr, tlc+shift, brr, brc+shift)
-        diamondFill(L, tlr-shift, tlc, brr-shift, brc)
-        diamondFill(L, tlr+shift, tlc, brr+shift, brc)
-
-def diamondFill(L, tlr, tlc, brr, brc):
-    #print(f'diamondFill({L},{tlr},{tlc},{brr},{brc})')
-    shift = (brr - tlr)/2
-    cr = tlr + shift
-    cc = tlc + shift
-    if ((cr < 0)|(cr >= n)|(cc < 0)|(cc >= n)):
-        return
-    mean = 0.25*(m(tlr,cc)+m(cr,tlc)+m(cr,brc)+m(brr,cc))
-    mat[int(cr),int(cc)] = mean + np.random.normal()*2**(-H*(N-L))
-    if (L > 1):
-        squareFill(L-1, tlr, tlc, tlr+shift, tlc+shift)
-        squareFill(L-1, tlr, tlc+shift, tlr+shift, brc)
-        squareFill(L-1, tlr+shift, tlc, brr, tlc+shift)
-        squareFill(L-1, tlr+shift, tlc+shift, brr, brc)
-
-print("Filling now...")        
-squareFill(N, 0, 0, n, n)
 print("Matrix filled. Rendering...")
 
-# Just extract part to hide artifacts
-mat = mat[0:int(n/2),0:int(n/2)]
+# Just extract centre to hide edge artifacts
+mat = mat[(n//4):(3*n//4),(n//4):(3*n//4)]
 
+# Render
 from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')

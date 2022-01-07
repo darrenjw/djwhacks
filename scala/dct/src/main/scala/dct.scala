9294a4356ea4069da50df700bb0e964297aa714e
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2022-01-07 15:30:30 +0000

    reference DCT implementations all working

--- scala/dct/src/main/scala/dct.scala
+++ scala/dct/src/main/scala/dct.scala
@@ -5,6 +5,7 @@ Stub for Scala Breeze code
 
 import breeze.linalg.*
 import breeze.numerics.*
+import breeze.signal.*
 import breeze.math.*
 import math.Pi
 
@@ -22,30 +23,52 @@ object RefImps:
 
   def idft(x: DenseVector[Complex]): DenseVector[Complex] = dftc(x, true)
 
+  // Naive DCT implementation for reference (O(n^2))
+  def dct0(x: DenseVector[Double]): DenseVector[Double] =
+    val N = x.length
+    val cf = DenseVector.tabulate(N){n => Pi*(n + 0.5)/N}
+    val X = DenseVector.tabulate(N){k => sum(x *:* cos(cf * k.toDouble))}
+    X * (2.0 / N)
+
+  // DCT implementation using the FFT from Breeze (O(n log n))
   def dct(x: DenseVector[Double]): DenseVector[Double] =
     val N = x.length
     val y = DenseVector.vertcat(x, x(N-1 to 0 by -1))
-    val Y = dft(y) // TODO: Switch to an FFT at some point...
+    val Y = fourierTr(y)
     val ff = DenseVector.tabulate(2*N){k => -Pi*i*k/(2*N)}
     val sY = Y *:* exp(ff)
     sY(0 to N-1).map(_.real) / N.toDouble
 
+  // Naive iDCT implementation for reference (O(n^2))
+  def idct0(x: DenseVector[Double]): DenseVector[Double] =
+    val N = x.length
+    val cf = DenseVector.tabulate(N-1){k => Pi*(k+1)/N}
+    DenseVector.tabulate(N){n => x(0)/2 + sum(x(1 to N-1) *:* cos(cf * (n+0.5)))}
+    
+  // iDCT implementation using the FFT from Breeze (O(n log n))
   def idct(x: DenseVector[Double]): DenseVector[Double] =
     val N = x.length
     val y = DenseVector.vertcat(x, DenseVector(0.0), -x(N-1 to 1 by -1))
     val ff = DenseVector.tabulate(2*N){k => Pi*i*k/(2*N)}
     val sy = y.map(Complex(_,0.0)) *:* exp(ff)
-    val Y = idft(sy) // TODO: Switch to an FFT at some point
+    val Y = iFourierTr(sy)
     Y(0 to N-1).map(_.real) * N.toDouble
 
-  def idct2d(X: DenseMatrix[Double]): DenseMatrix[Double] = ???
+  // 2d DCT and inversion using 1d on rows and columns
+  def dct2(X: DenseMatrix[Double], inverse: Boolean = false): DenseMatrix[Double] =
+    val x = X.copy
+    (0 until x.rows).foreach{j =>
+      x(j, ::) := (if (inverse) idct(x(j, ::).t)
+        else dct(x(j, ::).t)).t}
+    (0 until x.cols).foreach{k =>
+      x(::, k) := (if (inverse) idct(x(::, k))
+        else dct(x(::, k)))}
+    x
 
-
-  def main(args: Array[String]): Unit =
+  @main def examples() =
     val x = DenseVector(1.0,2.0,3.0,2.0,4.0,3.0)
     println(x)
     // try breeze built-in fft
-    import breeze.signal.*
     val X = fourierTr(x)
     println("Breeze FFT")
     println(X)
@@ -56,14 +79,23 @@ object RefImps:
     println(iFourierTr(fourierTr(x)).map(_.real))
     println("My inversion")
     println(idft(dft(x)).map(_.real))
+    println("DCT0")
+    println(dct0(x))
     println("DCT")
     println(dct(x))
-    println("Inversion")
+    println("iDCT0")
+    println(idct0(x))
+    println("iDCT")
+    println(idct(x))
+    println("DCT0 Inversion")
+    println(idct0(dct0(x)))
+    println("DCT Inversion")
     println(idct(dct(x)))
     // 2d stuff
     // try breeze
     val M = DenseMatrix((1.0,2.0,3.0),(2.0,2.0,2.0))
     println(M)
     println(fourierTr(M))
-
+    println(dct2(M))
+    println(dct2(dct2(M), true))
 

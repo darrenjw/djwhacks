8707bd9e908750cd8b5e0bcd58ee29faab24a2c9
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2022-03-23 17:07:43 +0000

    building free PPs

--- scala/ppl3/src/main/scala/free-ppl.scala
+++ scala/ppl3/src/main/scala/free-ppl.scala
@@ -5,12 +5,15 @@ free-ppl.scala
 
  */
 
+import cats.*
+import cats.syntax.*
+
 object FreePPL:
 
   enum Dist[A]:
-    //case Observe(d: Dist[A], o: A) extends Dist[A]
-    case Cond(ll: A => Double) extends Dist[A]
-    case Normal(m: Double, s: Double) extends Dist[Double]
+    case Observe(d: Dist[A], o: A) extends Dist[A]
+    //case Cond(ll: A => Double) extends Dist[A]
+    case Normal(m: Double, v: Double) extends Dist[Double]
     case Poisson(l: Double) extends Dist[Int]
     case Gamma(a: Double, b: Double) extends Dist[Double]
   import Dist.*
@@ -18,15 +21,29 @@ object FreePPL:
   import cats.free.Free
   type DistF[A] = Free[Dist, A]
 
-  // def observe[A](d: Dist[A], o: A) = Free.liftF[Dist, A](Observe(d, o))
-  def cond[A](ll: A => Double) = Free.liftF[Dist, A](Cond(ll))
+  def observe[A](d: Dist[A], o: A) = Free.liftF[Dist, A](Observe(d, o))
+  //def cond[A](ll: A => Double) = Free.liftF[Dist, A](Cond(ll))
   def normal(m: Double, s: Double) = Free.liftF[Dist, Double](Normal(m, s))
   def poisson(l: Double) = Free.liftF[Dist, Int](Poisson(l))
   def gamma(a: Double, b: Double) = Free.liftF[Dist, Double](Gamma(a, b))
 
+object FreePPL3:
+
+  import PPL3.Prob
+  import FreePPL.*
+  import Dist.*
+
+  val c2smc = new (Dist ~> PPL3.Prob):
+    def apply[A](pa: Dist[A]): Prob[A] = pa match
+      case Normal(m: Double, v: Double) => PPL3.Normal(m, v)
+      case Poisson(l: Double) => PPL3.Poisson(l)
+      case Gamma(a: Double, b: Double) => PPL3.Gamma(a, b)
+      //case Observe(d: Dist[A], o: A) => apply(d).fitQ(o)
+
 object FreeExample:
 
   import FreePPL.*
+  import Dist.*
 
   val prior1 = for
     x <- normal(0,1)
@@ -56,13 +73,16 @@ object FreeExample:
       tup <- predict
       (w, states) = tup
       st = states.head
-      //_ <- observe(poisson(math.exp(st)), obs)
+      _ <- observe(Poisson(math.exp(st)), obs)
     yield (w, states)
     updated
 
   val mod = data.foldLeft(prior)(addTimePoint(_,_))
 
 
+  @main def runFree() =
+    println("running")
+    println(mod)
 
 
 // eof

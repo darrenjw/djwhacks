463f199cd18137156d3fe3ecebd0d438202b5479
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2023-02-06 14:39:14 +0000

    working on implementation

--- scala/hmm/runHmm.R
+++ scala/hmm/runHmm.R
@@ -9,11 +9,22 @@ plot(ts(x))
 P = rbind(c(0.9, 0.1), c(0.1, 0.9))
 
 ## Use forward algorithm to compute filtered probabilities
-la = forward(x, P, c(0.5,0.5), "norm", list(mean=c(0, 3)))
+la = forward(x, P, c(0.5, 0.5), "norm", list(mean=c(0, 3)))
 a = exp(la)
 norm = apply(a, 1, sum)
 fil = a/norm
+print("Filtered")
 print(fil)
 plot(ts(fil))
+
+## Use forward-backward algorithm to compute smoothed probabilities
+lab = forwardback(x, P, c(0.5, 0.5), "norm", list(mean=c(0, 3)))
+labs = lab$logalpha + lab$logbeta
+ab = exp(labs) # TODO: could/should logsumexp this properly!
+norm = apply(ab, 1, sum)
+smo = ab / norm
+print("Smoothed")
+print(smo)
+plot(ts(smo))
 
 ## eof
--- scala/hmm/src/main/scala/hmm.scala
+++ scala/hmm/src/main/scala/hmm.scala
@@ -11,24 +11,31 @@ import cats.effect.{IOApp, IO, ExitCode}
 
 import breeze.linalg.*
 import breeze.numerics.*
+import breeze.stats.distributions.Gaussian
+import breeze.stats.distributions.Rand.VariableSeed.randBasis
+
+type DVD = DenseVector[Double]
+type DMD = DenseMatrix[Double]
 
 object HmmApp extends IOApp.Simple:
 
-  def readData : IO[List[Double]] = IO {
+  def readData : IO[List[Double]] = IO(
     scala.io.Source.fromFile("short.txt").getLines.toList.map(_.toDouble)
-  }
+  )
 
+  def forwardStep[A](P: DMD, f: A => DVD)(pi: DVD, a: A): DVD =
+    val unn = f(a) *:* (P.t * pi)
+    unn / sum(unn)
 
-  def forwardStep[A](x: A, pi: DenseVector[Double], P: DenseMatrix[Double],
-      f: (Int, A) => Double): DenseVector[Double] = ???
-
-  def forward[A](x: List[A], pi0: DenseVector[Double])(
-      f: (Int, A) => Double)(P: DenseMatrix[Double]) : List[DenseVector[Double]] = ???
-
-
+  def forward[A](la: List[A], pi0: DVD)(f: A => DVD)(P: DMD) : List[DVD] =
+    la.scanLeft(pi0)(forwardStep(P, f))
 
 
   def run = for
     x <- readData
     _ <- IO.println(x)
+    fil = forward(x, DenseVector(0.5, 0.5))(
+      (x: Double) => DenseVector(Gaussian(0.0, 1.0).pdf(x), Gaussian(3.0, 1.0).pdf(x)))(
+      DenseMatrix((0.9, 0.1), (0.1, 0.9)))
+    _ <- IO.println(fil)
   yield ExitCode.Success

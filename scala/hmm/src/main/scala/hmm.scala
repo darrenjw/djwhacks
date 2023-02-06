e97f8d00a754f11261441cd6f5e5d1275d522e98
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2023-02-06 17:39:41 +0000

    forward-backward smoothing working in scala

--- scala/hmm/src/main/scala/hmm.scala
+++ scala/hmm/src/main/scala/hmm.scala
@@ -27,15 +27,32 @@ object HmmApp extends IOApp.Simple:
     val unn = f(a) *:* (P.t * pi)
     unn / sum(unn)
 
-  def forward[A](la: List[A], pi0: DVD)(f: A => DVD)(P: DMD) : List[DVD] =
+  def forward[A](la: List[A], pi0: DVD)(f: A => DVD)(P: DMD): List[DVD] =
     la.scanLeft(pi0)(forwardStep(P, f))
 
+  def backStep[A](P: DMD)(s: DVD, f: DVD): DVD =
+    f *:* (P * (s /:/ (P.t * f)))
+
+  def back[A](fpl: List[DVD])(P: DMD): List[DVD] =
+    val rev = fpl.drop(1).reverse
+    val init = rev.head
+    rev.drop(1).scanLeft(init)(backStep(P)).reverse
+
+  def smooth[A](la: List[A], pi0: DVD)(f: A => DVD)(P: DMD): List[DVD] =
+    back(forward(la, pi0)(f)(P))(P)
+
+
+
+
+
+  val pi0 = DenseVector(0.5, 0.5)
+  val P = DenseMatrix((0.9, 0.1), (0.1, 0.9))
 
   def run = for
     x <- readData
     _ <- IO.println(x)
-    fil = forward(x, DenseVector(0.5, 0.5))(
+    smo = smooth(x, pi0)(
       (x: Double) => DenseVector(Gaussian(0.0, 1.0).pdf(x), Gaussian(3.0, 1.0).pdf(x)))(
-      DenseMatrix((0.9, 0.1), (0.1, 0.9)))
-    _ <- IO.println(fil)
+      P)
+    _ <- IO.println(smo)
   yield ExitCode.Success

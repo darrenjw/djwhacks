de2d20de51d723635353e096dfca4e7f5ac0a895
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2023-03-01 19:22:30 +0000

    minor refactor and add a few tests

--- scala/tsqr/src/main/scala/tsqr.scala
+++ scala/tsqr/src/main/scala/tsqr.scala
@@ -11,6 +11,7 @@ import cats.effect.{IO, IOApp}
 
 import breeze.linalg.*
 import breeze.numerics.*
+import dev.ludovic.netlib.blas.BLAS.{ getInstance => blas }
 
 import scala.concurrent.{ Future, Await, ExecutionContext }
 import scala.concurrent.duration.*
@@ -20,17 +21,17 @@ import ExecutionContext.Implicits.global
 type DMD = DenseMatrix[Double]
 
 
-object TsqrApp extends IOApp.Simple:
+object Tsqr:
 
-
-  def matClose(a: DMD, b: DMD, tol: Double = 1.0e-3): Boolean =
-    val diff = a - b
-    (sum(diff *:* diff) < tol)
+  def forwardSolve(A: DMD, Y: DMD): DMD =
+    val yc = Y.copy
+    blas.dtrsm("L", "L", "N", "N", yc.rows, yc.cols, 1.0, A.toArray,
+      A.rows, yc.data, yc.rows)
+    yc
 
   @annotation.tailrec
-  def tsqr(x: DMD, blockSize: Int = 2000, threshold: Int = 5000): DMD =
+  def tsqR(x: DMD, blockSize: Int = 2500, threshold: Int = 10000): DMD =
     val n = x.rows
-    val m = x.cols
     if (n < threshold) qr.justR(x) else
       val blocks = if (n % blockSize == 0) (n / blockSize) else (n / blockSize + 1)
       val rMatsF = (0 until blocks).toVector.
@@ -39,7 +40,26 @@ object TsqrApp extends IOApp.Simple:
         map(fm => fm.map(m => qr.justR(m))).sequence
       val newXF = rMatsF.map(rMats => rMats.reduce(DenseMatrix.vertcat(_, _)))
       val newX = Await.result(newXF, 10.seconds)
-      tsqr(newX, blockSize, threshold)
+      tsqR(newX, blockSize, threshold)
+
+  def tsQr(x: DMD, r: DMD, blockSize: Int = 20000, threshold: Int = 30000): DMD =
+    val n = x.rows  
+    if (n < threshold) forwardSolve(r.t, x.t).t else
+      val blocks = if (n % blockSize == 0) (n / blockSize) else (n / blockSize + 1)
+      val qMatsF = (0 until blocks).toVector.
+        map(i => Future(i)).
+        map(fi => fi.map(i => x(i*blockSize until math.min(n, (i+1)*blockSize), ::))).
+        map(fm => fm.map(m => forwardSolve(r.t, m.t).t)).sequence
+      val qF = qMatsF.map(qMats => qMats.reduce(DenseMatrix.vertcat(_, _)))
+      Await.result(qF, 10.seconds)
+
+  def tsQR(x: DMD): (DMD, DMD) =
+    val r = tsqR(x)
+    (tsQr(x, r), r)
+
+  def matClose(a: DMD, b: DMD, tol: Double = 1.0e-3): Boolean =
+    val diff = a - b
+    (sum(diff *:* diff) < tol)
 
   def time[A](f: => A): A =
     val s = System.nanoTime
@@ -47,14 +67,34 @@ object TsqrApp extends IOApp.Simple:
     println("time: "+(System.nanoTime - s)/1e6+"ms")
     ret
 
-  val m = DenseMatrix.rand(10000, 20)
+
+object TsqrApp extends IOApp.Simple:
+
+  import Tsqr.*
+
+  val m = DenseMatrix.rand(100000, 50)
 
   val r1 = time { qr.justR(m) }
  
-  val r2 = time { tsqr(m) }
+  val r2 = time { tsqR(m) }
+
+  val r3 = time { qr.justR(m) }
+
+  val q1 = time { forwardSolve(r2.t, m.t).t }
+
+  val q2 = time { tsQr(m, r2) }
+
+  val q3 = time { forwardSolve(r2.t, m.t).t }
+
+  val qr1 = time { qr.reduced(m) }
+ 
+  val qr2 = time { tsQR(m) }
+
 
   def run =
     for
       _ <- IO.println( r2.rows )
       _ <- IO.println( matClose(abs(r1), abs(r2)) )
+      _ <- IO.println( matClose(qr2._1 * qr2._2, m) )
+      _ <- IO.println( matClose(q2.t * q2, DenseMatrix.eye[Double](q2.cols)) )
     yield ()
--- scala/tsqr/src/test/scala/tsqr-unit.scala
+++ scala/tsqr/src/test/scala/tsqr-unit.scala
@@ -1,15 +1,41 @@
 import cats.*
 import cats.implicits.*
 
+import breeze.linalg.*
+import breeze.numerics.*
+
 import munit.*
 
 // Example unit tests
-class MyUnitTests extends FunSuite:
+class TsqrUnitTests extends FunSuite:
 
-  test("A List should combine") {
-    val l = List(1,2) |+| List(3,4)
-    assert(l === List(1,2,3,4))
+  import Tsqr.*
+
+  def matClose(a: DMD, b: DMD, tol: Double = 1.0e-5): Boolean =
+    val diff = a - b
+    (sum(diff *:* diff) < tol)
+
+
+  val x = DenseMatrix.rand(100000, 50)
+
+  val (q, r) = tsQR(x)
+
+  test("q and r should combine to give x") {
+    assert(matClose(q * r, x))
   }
+
+  test("q should have orthonormal columns") {
+    assert(matClose(q.t * q, DenseMatrix.eye[Double](q.cols)))
+  }
+
+  test("r should be upper triangular") {
+    assert(matClose(r, upperTriangular(r)))
+  }
+
+  test("r should agree with breeze function up to signs") {
+    assert(matClose(abs(r), abs(qr.justR(x))))
+  }
+
 
 
 // eof

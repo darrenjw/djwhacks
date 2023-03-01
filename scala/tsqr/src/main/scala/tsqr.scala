f00981d65732192710634e05f37bc62cc239bdd7
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2023-03-01 16:15:16 +0000

    tsqr running in parallel and providing good speedup

--- scala/tsqr/src/main/scala/tsqr.scala
+++ scala/tsqr/src/main/scala/tsqr.scala
@@ -12,6 +12,11 @@ import cats.effect.{IO, IOApp}
 import breeze.linalg.*
 import breeze.numerics.*
 
+import scala.concurrent.{ Future, Await, ExecutionContext }
+import scala.concurrent.duration.*
+import ExecutionContext.Implicits.global
+
+
 type DMD = DenseMatrix[Double]
 
 
@@ -27,17 +32,25 @@ object TsqrApp extends IOApp.Simple:
     val m = x.cols
     if (n < threshold) qr.justR(x) else
       val blocks = if (n % blockSize == 0) (n / blockSize) else (n / blockSize + 1)
-      val rMats = (0 until blocks).toVector.
-        map(i => x((i*blockSize) until math.min(n, (i+1)*blockSize), ::)).
-        map(m => qr.justR(m))
-      val newX = rMats.reduce(DenseMatrix.vertcat(_, _))
+      val rMatsF = (0 until blocks).toVector.
+        map(i => Future(i)).
+        map(fi => fi.map(i => x((i*blockSize) until math.min(n, (i+1)*blockSize), ::))).
+        map(fm => fm.map(m => qr.justR(m))).sequence
+      val newXF = rMatsF.map(rMats => rMats.reduce(DenseMatrix.vertcat(_, _)))
+      val newX = Await.result(newXF, 10.seconds)
       tsqr(newX, blockSize, threshold)
 
-  val m = DenseMatrix.rand(10000, 50)
+  def time[A](f: => A): A =
+    val s = System.nanoTime
+    val ret = f
+    println("time: "+(System.nanoTime - s)/1e6+"ms")
+    ret
 
-  val r1 = qr.justR(m)
+  val m = DenseMatrix.rand(10000, 20)
+
+  val r1 = time { qr.justR(m) }
  
-  val r2 = tsqr(m)
+  val r2 = time { tsqr(m) }
 
   def run =
     for

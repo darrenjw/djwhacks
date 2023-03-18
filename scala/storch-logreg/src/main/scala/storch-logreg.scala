86e5f1a9eb1eaf5ddf5ab28d0683929d0c519b2f
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2023-03-18 16:32:39 +0000

    initial template

---
+++ scala/storch-logreg/src/main/scala/storch-logreg.scala
@@ -0,0 +1,15 @@
+/*
+storch-logreg.scala
+Stub for Scala Cats code
+*/
+
+import cats.*
+import cats.implicits.*
+import cats.effect.{IO, IOApp}
+
+object CatsApp extends IOApp.Simple:
+
+  val l = List(1,2) |+| List(3,4)
+
+  def run = IO{ println(l) }
+

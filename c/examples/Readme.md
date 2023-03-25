1807a665a2c24ecde2d888c00cddd6ff99b6b2f4
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2023-03-25 18:50:58 +0000

    add a sin example

--- c/examples/Readme.md
+++ c/examples/Readme.md
@@ -6,10 +6,10 @@ In increasing order of complexity:
 * [prime.c](prime.c) - test if a number (provided as a command line arg) is prime (separate function)
 * [factor.c](factor.c) - factor a number into its prime factors (recursive function)
 * [sqrt.c](sqrt.c) - square root a number using Newton's method (while loop)
+* [sin.c](sin.c) - compute the sin of a number (in radians) using trig (recursive function)
 
 ## Some ideas for extra examples
 
-* Sin via recursion
 * Dereferenced typdefed structs...
 * A simple canvas drawing app, with a bunch of examples (sierpinski triangles, menger sponge, fractal trees, GoL, Langton's ant, etc.), but just writing image to disk
 * Using an external library - eg. an image library?
---
+++ c/examples/sin.c
@@ -0,0 +1,47 @@
+/*
+sin.c
+
+Compute the sin of a number using the trig identity:
+
+sin(x) = 3sin(x/3) - 4sin^3(x/3)
+
+and the fact that for small enough x, sin(x) ~= x.
+
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+double mySin(double);
+
+int main(int argc, char *argv[]) {
+  double x, sx;
+  char *junk;
+  if (argc != 2) {
+    printf("Usage: sin <number>\n");
+    exit(1);
+  }
+  printf("Parsing input...\n");
+  x = strtod(argv[1], &junk);
+  printf("Read: %f.\n", x);
+  sx = mySin(x);
+  printf("Sin is: %f.\n", sx);
+  exit(0);
+}
+
+
+double mySin(double x) {
+  double sx3;
+  if (x < 1.0e-5)
+    return(x);
+  else {
+    sx3 = mySin(x/3);
+    return(3*sx3 - 4*sx3*sx3*sx3);
+  }
+}
+
+
+    
+/* eof */
+

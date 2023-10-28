fc24bc780da7480c522232e900bf038bd9d19824
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2023-10-28 23:24:08 +0100

    a bit more smfsb

--- python/smfsb.py
+++ python/smfsb.py
@@ -41,6 +41,9 @@ class Spn:
                 j = np.random.choice(v, p=h/h0)
                 x = np.add(x, S[:,j].A1)
         return step
+
+# TODO: want stepPTS, too
+
     
 # some simulation functions
 
@@ -57,31 +60,56 @@ def simTs(x0, t0, tt, dt, stepFun):
         mat[i,:] = x
     return mat
 
-# TODO: want simSample, too
+def simSample(n, x0, t0, deltat, stepFun):
+    u = len(x0)
+    mat = np.zeros((n, u))
+    for i in range(n):
+        mat[i,:] = stepFun(x0, t0, deltat)
+    return mat
+
 
 # some example SPN models
     
 lv = Spn([[1,0],[1,1],[0,1]], [[2,0],[0,2],[0,0]],
          lambda x, t: np.array([x[0], 0.005*x[0]*x[1], 0.6*x[1]]),
-                               [50,100])
-stepLv = lv.stepGillespie()
+         [50,100])
+
+sir = Spn([[1,1,0],[0,1,0]], [[0,2,0],[0,0,1]],
+          lambda x, t: np.array([0.1*x[0]*x[1], x[2]]),
+          [100,1,0])
 
 # some test code
+if __name__ == '__main__':
+    stepLv = lv.stepGillespie()
+    stepSir = sir.stepGillespie()
 
-print(lv)
-print(lv.m)
-print(stepLv(lv.m, 0, 1.0))
-print(stepLv(lv.m, 0, 1.0))
-         
-out = simTs(lv.m, 0, 100, 0.1, stepLv)
+    print(lv)
+    print(lv.m)
+    print(stepLv(lv.m, 0, 1.0))
+    print(stepLv(lv.m, 0, 1.0))
 
-import matplotlib.pyplot as plt
-figure, axis = plt.subplots(2)
-for i in range(2):
-    axis[i].plot(range(out.shape[0]), out[:,i])
-    axis[i].set_title(f'Time series for variable {i}')
-plt.savefig("lv-ts.png")
+    print("First generate a time series")
+    out = simTs(lv.m, 0, 100, 0.1, stepLv)
 
+    import matplotlib.pyplot as plt
+    figure, axis = plt.subplots(2)
+    for i in range(2):
+        axis[i].plot(range(out.shape[0]), out[:,i])
+        axis[i].set_title(f'Time series for variable {i}')
+    plt.savefig("lv-ts.png")
 
+    print("Next look at a transition kernel (slow)")
+    mat = simSample(1000, lv.m, 0, 10, stepLv)
+    figure, axis = plt.subplots(2)
+    for i in range(2):
+        axis[i].hist(mat[:,i],30)
+        axis[i].set_title(f'Histogram for variable {i}')
+    plt.savefig("lv-hist.png")
+
+    # TODO: SIR test
+
+    # TODO: PTS test
+
+    
 # eof
 

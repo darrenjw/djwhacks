305d335eaabf52887bf9633b3c580b7e4870cf2f
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2023-11-24 09:45:39 +0000

    simulated data

--- python/sir.py
+++ python/sir.py
@@ -5,8 +5,8 @@ import scipy as sp
 import numpy as np
 
 N = 200
-beta = 0.1
-gamma = 0.2
+beta = 0.3
+gamma = 0.1
 
 init = np.array([N, 1])
 
@@ -20,6 +20,23 @@ print(rhs(0, init))
 out = sp.integrate.solve_ivp(rhs, (0, 100), init, t_eval=range(100))
 
 print(out)
+#print(out.t)
+#print(out.y)
+
+import matplotlib.pyplot as plt
+figure, axis = plt.subplots(2)
+for i in range(2):
+    axis[i].plot(out.t, out.y[i,:])
+    axis[i].set_title(f'Time series for SIR variable {i}')
+plt.savefig("sir-ts.png")
+
+
+# Now attempt to recover beta and gamma parameters
+# based on observations of I only
+
+
+
+
 
 # eof
 

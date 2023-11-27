d7fad139ba5e40f0471c131f5d0b61d59401a04b
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2023-11-27 08:51:30 +0000

    alternative SIR fitting script

--- python/sir2.py
+++ python/sir2.py
@@ -1,4 +1,4 @@
-# sir.py
+# sir2.py
 # Stochastic and deterministic SIR model, and fitting to data
 
 import scipy as sp
@@ -16,35 +16,35 @@ i0 = 3 # initial number of infected
 init = np.array([N - i0, i0])
 
 # Do a stochastic simulation using the Gillespie algorithm
-# Record state on a regular time grid (eg. daily observations)
-gout = np.zeros((100, 2))
+# Record time and state for every event
+gstates = np.zeros((2*N, 2)) # 2N is an upper bound on number of events
+gtimes = np.zeros(2*N)
 t = 0; i = 0; x = init.copy()
-for i in range(100):
-    if (t >= i):
-        gout[i,:] = x
+gstates[i,:] = x
+while (x[1] > 0):
+    i = i + 1
+    si = beta*x[0]*x[1]/N
+    ir = gamma*x[1]
+    h0 = si + ir
+    if (h0 < 1e-8):
+        t = 1e99
     else:
-        while(True):
-            si = beta*x[0]*x[1]/N
-            ir = gamma*x[1]
-            h0 = si + ir
-            if (h0 < 1e-8):
-                t = 1e99
-            else:
-                t = t + np.random.exponential(1.0/h0)
-            if ((t >= i)|(h0 > 1e6)):
-                gout[i,:] = x
-                break
-            if (np.random.random() < si/h0):
-                x[0] = x[0] - 1
-                x[1] = x[1] + 1
-            else:
-                x[1] = x[1] - 1
-
+        t = t + np.random.exponential(1.0/h0)
+    if (np.random.random() < si/h0):
+        x[0] = x[0] - 1
+        x[1] = x[1] + 1
+    else:
+        x[1] = x[1] - 1
+    gtimes[i] = t
+    gstates[i,:] = x
+gtimes = gtimes[range(i+1)]
+gstates = gstates[range(i+1),:]
+    
 # Plot the stochastic realisation
 import matplotlib.pyplot as plt
 figure, axis = plt.subplots(2)
 for i in range(2):
-    axis[i].plot(range(100), gout[:,i])
+    axis[i].plot(gtimes, gstates[:,i])
     axis[i].set_title(f'Time series for SIR variable {i}')
 plt.savefig("sir-ts-stoch.png")
 
@@ -73,17 +73,22 @@ plt.savefig("sir-ts-ode.png")
 
 # Now pretend that we don't know the paramters and attempt to
 # recover beta and gamma parameters based on stochastic observations
-data = gout
-print(data)
 
+# This is all a bit dumb, since if we have perfect observations we can
+# exactly compute the MLEs for the parameters for the original stochastic model...
+
+data = gstates.T
+
+# squared error loss at observation times
+# note that this is not a (good) estimate of the l2 distance between the curves
+# since observations are not uniformly distributed in time
 def loss(bg):
     print(bg)
     beta = bg[0]
     gamma = bg[1]
     out = sp.integrate.solve_ivp(sir(beta, gamma),
-                (0, 100), init, t_eval=range(100))
-    simI = out.y.T
-    diff = simI - data
+                    (0, gtimes[-1]), init, t_eval=gtimes)
+    diff = out.y - data
     l = np.sum(diff*diff)
     print(l)
     return l

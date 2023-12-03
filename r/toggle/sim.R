b3fc2fd09c857b00268393a516e729f8cb893df3
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2023-12-03 21:53:26 +0000

    model matching results in paper

--- r/toggle/sim.R
+++ r/toggle/sim.R
@@ -13,6 +13,7 @@ mod = sbml2spn("lambda.xml")
 step = StepGillespie(mod)
 
 ## Create and plot a single realisation of the process
+print("Single realisation")
 out = simTs(mod$M, 0, 100, 0.1, step)
 colours = rainbow(ncol(out))
 plot.ts(out, plot.type="single", lwd=2, col=colours,
@@ -23,9 +24,23 @@ legend(0, max(out), colnames(out), lwd=2, lty=1,
        col=colours, bg="white")
 
 ## Look at the levels of CII at time 60
-out = simSample(750, mod$M, 0, 60, step)
+print("Sample at time 60")
+out = simSample(250, mod$M, 0, 60, step)
 cii = out[,5]
 hist(cii, 30, main="CII at time 60 minutes")
+
+## Look at average over time up to 60
+print("Average up to time 60")
+n = 5000
+v = vector("numeric", n)
+for (i in 1:n) {
+    out = simTs(mod$M, 0, 60, 0.1, step)
+    v[i] = mean(out[,5])
+}
+hist(v, 30, main="CII averaged over 60 minutes")
+print(mean(v))
+print(sd(v))
+
 
 
 ## eof

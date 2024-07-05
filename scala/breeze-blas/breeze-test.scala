2207c322ba6ab72f6c8303565180b9f03e997d48
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2024-07-05 17:40:37 +0100

    blas test working

--- scala/breeze-blas/breeze-test.scala
+++ scala/breeze-blas/breeze-test.scala
@@ -1,8 +1,9 @@
 //> using scala 3.3.0
 //> using dep org.scalanlp::breeze:2.1.0
 //> using dep org.scalanlp::breeze-viz:2.1.0
-//> using javaOpt -Ddev.ludovic.netlib.blas.nativeLibPath=/usr/lib/x86_64-linux-gnu/blas/libblas.so.3.10.0
 
+
+// > using javaOpt -Ddev.ludovic.netlib.blas.nativeLibPath=/usr/lib/x86_64-linux-gnu/blas/libblas.so.3.10.0
 // > using javaOpt -Ddev.ludovic.netlib.blas.nativeLib=libblas.so
 
 
@@ -21,13 +22,22 @@
 // -Ddev.ludovic.netlib.blas.nativeLibPath=/usr/lib/x86_64-linux-gnu/blas/libblas.so.3.10.0
 // -Ddev.ludovic.netlib.blas.nativeLib=libblas.so
 
+
+import breeze.linalg.*
+import dev.ludovic.netlib.blas.BLAS
+
 object Main:
 
   @main
   def run() = 
-    import dev.ludovic.netlib.blas.BLAS
     println(BLAS.getInstance().getClass().getName())
-
+    println(DenseMatrix((1.0,0.0),(0.2,2.0)) * DenseVector(3.0, 4.0))
+    println(BLAS.getInstance().getClass().getName())
+    println("\n\n\n")
+    BLAS.getInstance().getClass().getName() match
+      case "dev.ludovic.netlib.blas.Java8BLAS" => println("Fallen back to Java BLAS")
+      case "dev.ludovic.netlib.blas.JNIBLAS" => println("Using a native BLAS of some sort")
+    println("\n\n\n")
 
 
 // eof

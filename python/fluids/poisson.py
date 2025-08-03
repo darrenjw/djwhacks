4eff2e690cfbbf829020e0d8136802d67dbaed38
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2025-08-03 16:31:11 +0100

    added 1d FEM solution of the poisson equation

--- python/fluids/poisson.py
+++ python/fluids/poisson.py
@@ -6,9 +6,10 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
+# use a simple (tractable) RHS
 x = np.arange(0, 1, 0.001)
 b = np.sin(2*math.pi*x) # easy RHS
-fig, axis = plt.subplots(3) # set number of subplots here
+fig, axis = plt.subplots(4) # set number of subplots here
 axis[0].plot(x, b)
 axis[0].set_title("RHS")
 
@@ -22,15 +23,27 @@ axis[1].plot(x, p)
 axis[1].set_title("FFT solution")
 
 # now using a sparse matrix (for discrete possion equation)
+# sparse matrix for discrete laplacian
 lap = sp.sparse.diags_array([np.full((1000), -2),
                            np.full((999), 1), np.full((999), 1)],
                             offsets = [0, 1, -1], format = "csr")
-lap = lap / (0.001**2)
+lap = lap / (0.001**2) # scale for grid spacing
 p = sp.sparse.linalg.spsolve(lap, b)
 axis[2].plot(x, p)
 axis[2].set_title("Sparse solution")
 
-# TODO: add a 1d FEM solution...
+# 1d FEM solution...
+L = sp.sparse.diags_array([np.full((1000), 0.002/0.001/0.001),
+                           np.full((999), -1/0.001),
+                           np.full((999), -1/0.001)],
+                           offsets = [0, 1, -1], format="csr")
+M = sp.sparse.diags_array([np.full((1000), 0.002/3),
+                           np.full((999), 0.001/6),
+                           np.full((999), 0.001/6)],
+                           offsets = [0, 1, -1], format="csr")
+p = sp.sparse.linalg.spsolve(L, -M@b)
+axis[3].plot(x, p)
+axis[3].set_title("FEM solution")
 
 
 # save figure

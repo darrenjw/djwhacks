261ee2c8e1d2386755c741d965f6e3c7ae1b338e
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2025-12-13 19:08:43 +0000

    added an eigen-solver

--- python/lyap/dlyap.py
+++ python/lyap/dlyap.py
@@ -2,7 +2,7 @@
 # dlyap.py
 # solve the discrete lyapunov equation in JAX
 
-# solve AXA' - X + Q = 0  for X (symmetric Q)
+# solve AXA' - X + Q = 0  for X
 
 import jax
 import jax.numpy as jnp
@@ -17,7 +17,6 @@ k0 = jax.random.key(24)
 k1, k2 = jax.random.split(k0)
 A = jax.random.normal(k1, (n, n))
 Q = jax.random.normal(k2, (n, n))
-Q = Q * Q.T  # PSD Q
 
 
 # function to test a solution
@@ -29,7 +28,8 @@ def test_dlyap(a_mat, q_mat, x_mat, verb=True, tol=1.0e-8):
     return n < tol
 
 
-# simple kronecker based function to start with (scales badly)
+print("First try a kronecker solution")
+
 @jax.jit
 def dlyap_k(a_mat, q_mat):
     n = A.shape[0]
@@ -41,21 +41,31 @@ def dlyap_k(a_mat, q_mat):
 Xk = dlyap_k(A, Q)
 print(test_dlyap(A, Q, Xk))
 
+print("Next use the sylvester solver")
 
-# *********************************************************
-# JAX function to solve the discrete lyapunov equation
-# AXA' - X + Q = 0  for X  (efficiently)
 @jax.jit
-def dlyap(a_mat, q_mat):
+def dlyap_s(a_mat, q_mat):
     n = a_mat.shape[0]
     b_mat = jnp.linalg.solve(a_mat + jnp.eye(n), a_mat - jnp.eye(n))
     r_mat = 0.5 * (jnp.eye(n) - b_mat) @ q_mat @ (jnp.eye(n) - b_mat).T
     return jsp.linalg.solve_sylvester(b_mat, b_mat.T, -r_mat)
 
 
-# *********************************************************
-
-Xs = dlyap(A, Q)
+Xs = dlyap_s(A, Q)
 print(test_dlyap(A, Q, Xs))
+
+print("Next use an eigen-decomposition")
+
+@jax.jit
+def dlyap_e(a_mat, q_mat):
+    n = a_mat.shape[0]
+    e_vals, e_vecs = jnp.linalg.eig(a_mat)
+    f_mat = jnp.linalg.solve(e_vecs, (jnp.linalg.solve(e_vecs, q_mat.T)).T)
+    w_mat = e_vals[:, None]*e_vals[None, :] - 1
+    y_mat = -f_mat / w_mat
+    return (e_vecs @ y_mat @ e_vecs.T).real
+
+Xe = dlyap_e(A, Q)
+print(test_dlyap(A, Q, Xe))
 
 # eof
